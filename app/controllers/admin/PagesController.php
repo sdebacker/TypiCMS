fbdcc6b97d83aa90c2dddc72977578c730a52f1d
Samuel De Backer <sdebacker@gmail.com>
2013-10-31 16:36:27 +0100

    debug

--- app/controllers/admin/PagesController.php
+++ app/controllers/admin/PagesController.php
@@ -107,11 +107,12 @@ class PagesController extends BaseController {
 	public function update($model)
 	{
 		// Numeric values must be integer for checkboxes not to be checked.
+		d(Input::all());
 		$post = array();
 		foreach (Input::all() as $key => $value) {
 			$post[$key] = is_numeric($value) ? (int) $value : $value ;
 		}
-
+		d($post);
 		if ( ! Request::ajax()) {
 			if ( $this->form->update( $post ) ) {
 				return Redirect::route('admin.pages.index');
