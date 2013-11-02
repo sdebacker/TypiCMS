f1a72adfad4fbaf017945321a806526ec767e678
Samuel De Backer <sdebacker@gmail.com>
2013-11-02 11:24:30 +0100

    system info

--- app/views/admin/_footer.blade.php
+++ app/views/admin/_footer.blade.php
@@ -1,8 +1,17 @@
+	<hr>
 	<div class="row footer-main">
-		<ul>
-			<li>Environment: <b>{{ App::environment(); }}</b></li>
-			<li>setlocale(LC_ALL, 0): <b><?php echo setlocale(LC_ALL, 0); ?></b></li>
-			<li>Cache admin: <b><?php echo Config::get('typicms.cacheadmin') ? 'enabled': 'disabled'; ?></b></li>
-			<li>Cache public: <b><?php echo Config::get('typicms.cachepublic') ? 'enabled': 'disabled'; ?></b></li>
-		</ul>
+		<div class="col-sm-4 col-sm-offset-4">
+			<table class="table table-condensed">
+				<thead>
+					<tr><th colspan="2">System info</th></tr>
+				</thead>
+				<tr><td>Environment</td><td><b>{{ App::environment(); }}</b></td></tr>
+				<tr><td>setlocale(LC_ALL, 0)</td><td><b><?php echo setlocale(LC_ALL, 0); ?></b></td></tr>
+				<tr><td>Suported locales</td><td><b>{{ implode(', ', Config::get('app.locales')); }}</b></td></tr>
+				<tr><td>App locale</td><td><b>{{ Config::get('app.locale'); }}</b></td></tr>
+				<tr><td>Content locale</td><td><b>{{ Config::get('app.contentlocale'); }}</b></td></tr>
+				<tr><td>Cache admin</td><td><b><?php echo Config::get('typicms.cacheadmin') ? 'enabled': 'disabled'; ?></b></td></tr>
+				<tr><td>Cache public</td><td><b><?php echo Config::get('typicms.cachepublic') ? 'enabled': 'disabled'; ?></b></td></tr>
+			</table>
+		</div>
 	</div>
