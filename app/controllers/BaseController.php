0c3f8c075f8ca032fa5a45e1ddc768d4d7504902
Samuel De Backer <sdebacker@gmail.com>
2013-11-05 22:29:31 +0100

    languagesMenu() removed from baseController

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -4,26 +4,29 @@ use Controller;
 use View;
 use Config;
 use Request;
-use Route;
-use App;
-use DB;
 
 use TypiCMS\Models\Menulink;
+use TypiCMS\Services\MenuBuilder;
 use TypiCMS\Services\ListBuilder\ListBuilder;
 use Illuminate\Support\Collection;
 
 abstract class BaseController extends Controller {
 
-	public $applicationName;
+	/**
+	 * The layout that should be used for responses.
+	 */
+	protected $layout = 'public/master';
 
-	public $lang;
-
-	protected $model;
+	/**
+	 * Menus.
+	 */
+	protected $menuBuilder;
 	protected $mainMenu;
 	protected $footerMenu;
 
 	// The cool kids’ way of handling page titles.
 	// https://gist.github.com/jonathanmarvens/6017139
+	public $applicationName;
 	public $title  = array(
 		'parent'	=> '',
 		'separator' => '',
@@ -31,15 +34,10 @@ abstract class BaseController extends Controller {
 	);
 
 
-	/**
-	 * The layout that should be used for responses.
-	 */
-	protected $layout = 'public/master';
-
-
 	public function __construct($repository = null)
 	{
 		$this->repository = $repository;
+		$this->menuBuilder = new MenuBuilder;
 
 		// Si une langue est présente dans l'url
 		if ($lang = Request::segment(1)) {
@@ -62,14 +60,13 @@ abstract class BaseController extends Controller {
 		}
 
 		$instance = $this;
-
 		View::composer($this->layout, function ($view) use ($instance) {
 			$view->with('title', (implode(' ', $instance->title) . ' – ' . $instance->applicationName));
 		});
 
 		View::share('mainMenu', $this->mainMenu);
 		View::share('footerMenu', $this->footerMenu);
-		View::share('languagesMenu', $this->languagesMenu());
+		View::share('languagesMenu', $this->menuBuilder->languagesMenu());
 		View::share('lang', $lang);
 
 	}
@@ -86,117 +83,6 @@ abstract class BaseController extends Controller {
 		{
 			$this->layout = View::make($this->layout);
 		}
-	}
-
-
-	/**
-	 * Show resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($slug)
-	{
-		$model = $this->repository->bySlug($slug);
-
-		$this->title['parent'] = $model->title;
-		
-		$this->layout->content = View::make('public.'.$this->repository->view().'.show')
-			->with('model', $model);
-	}
-
-
-	/**
-	 * Build languages Menu.
-	 *
-	 * @return Collection
-	 */
-	private function languagesMenu()
-	{
-		$routeArray = explode('.',Route::currentRouteName());
-		$module = isset($routeArray[1]) ? $routeArray[1] : 'pages' ;
-		$segments = Request::segments();
-
-		// dd($routeArray);
-
-		if (end($routeArray) == 'slug') {
-			// Last segment is a slug
-			$slug = end($segments);
-			$id = DB::table($module)
-				->join($module.'_translations', $module.'.id', '=', $module.'_translations.'.str_singular($module).'_id')
-				->where('slug', $slug)
-				->pluck($module.'.id');
-			$slugs = DB::table($module)
-				->join($module.'_translations', $module.'.id', '=', $module.'_translations.'.str_singular($module).'_id')
-				->where($module.'.id', $id)
-				->where($module.'_translations.status', 1)
-				->lists('slug', 'lang');
-		}
-
-		if (isset($routeArray[2]) and $routeArray[2] == 'categories') {
-			// There is a category
-			if (end($routeArray) != 'categories') {
-				array_pop($segments); 
-			}
-			$category = end($segments);
-
-			$id = DB::table('categories')
-				->join('categories_translations', 'categories.id', '=', 'categories_translations.category_id')
-				->where('slug', $category)
-				->pluck('categories.id');
-			$categories = DB::table($module)
-				->join('categories', 'projects.category_id', '=', 'categories.id')
-				->join('categories_translations', 'categories_translations.category_id', '=', 'categories.id')
-				->where('categories.id', $id)
-				->where('categories_translations.status', 1)
-				->lists('slug', 'lang');
-		}
-
-		$languagesMenu = array();
-
-		if (count(Config::get('app.locales')) > 1) {
-
-			foreach (Config::get('app.locales') as $lg) {
-
-				// Build translated routes
-				$translatedRouteArray = $routeArray;
-				$translatedRouteArray[0] = $lg;
-
-				$translatedRouteName = implode('.', $translatedRouteArray);
-				$routeParams = array();
-
-				if (in_array('categories', $translatedRouteArray)) {
-					if (isset($categories[$lg])) {
-						$routeParams[] = $categories[$lg];
-					} else {
-						array_pop($translatedRouteArray);
-					}
-				}
-				if (in_array('slug', $translatedRouteArray)) {
-					if (isset($slugs[$lg])) {
-						$routeParams[] = $slugs[$lg];
-					} else {
-						array_pop($translatedRouteArray);
-					}
-				}
-				$translatedRouteName = implode('.', $translatedRouteArray);
-
-				// single page or module index
-				$route = Route::getRoutes()->get($translatedRouteName);
-				// if route in this lang doesn't exist, redirect to homepage
-				! $route and $translatedRouteName = $lg;
-
-				$languagesMenu[] = (object) array(
-					'lang' => $lg,
-					'url' => route($translatedRouteName, $routeParams),
-					'class' => Config::get('app.contentlocale') == $lg ? 'active' : ''
-				);
-
-			}
-
-		}
-
-		return $languagesMenu;
 	}
 
 }
