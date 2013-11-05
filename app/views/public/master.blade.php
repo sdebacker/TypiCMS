b92c57288e8e3289ba036ce402231800ea84eb70
Samuel De Backer <sdebacker@gmail.com>
2013-11-05 20:34:56 +0100

    on smartphones

--- app/views/public/master.blade.php
+++ app/views/public/master.blade.php
@@ -4,7 +4,7 @@
 <head>
 
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<title>{{ $title }}</title>
 
 	{{ basset_stylesheets('public') }}
