7609a2f512d9a2221b531f215567309e30a61eea
Samuel De Backer <sdebacker@gmail.com>
2013-11-07 16:25:52 +0100

    useless messages

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -12,19 +12,12 @@ class DatabaseSeeder extends Seeder {
 		Eloquent::unguard();
 
 		$this->call('SentrySeeder');
-		$this->command->info('Sentry tables seeded.');
 		$this->call('SettingsSeeder');
-		$this->command->info('Settings seeded.');
 		$this->call('PageSeeder');
-		$this->command->info('Pages seeded.');
 		$this->call('MenuSeeder');
-		$this->command->info('Menus seeded.');
 		$this->call('CategorySeeder');
-		$this->command->info('Categories seeded.');
 		$this->call('ProjectSeeder');
-		$this->command->info('Projects seeded.');
 		$this->call('EventSeeder');
-		$this->command->info('Events seeded.');
 	}
 
 }
