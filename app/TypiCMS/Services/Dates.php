9c2e9d6310123ac9ed6142ca1980ff3dda3f674a
Samuel De Backer <sdebacker@gmail.com>
2013-11-13 11:32:01 +0100

    dates service

---
+++ app/TypiCMS/Services/Dates.php
@@ -0,0 +1,38 @@
+<?php namespace TypiCMS\Services;
+
+use Carbon\Carbon;
+
+class Dates {
+
+	/**
+	 * Past events
+	 *
+	 * @return array
+	 */
+	public static function concat($startingDate, $endingDate)
+	{
+		
+		$startingDateArray = explode('-', $startingDate);
+		$endingDateArray = explode('-', $endingDate);
+		$endingDateFormat = '%A %d %B %Y';
+
+		if ($startingDate == $endingDate) {
+			$startingDateFormat = '%A %d %B %Y';
+			return Carbon::createFromFormat('Y-m-d', $startingDate)->formatLocalized($startingDateFormat);
+		}
+
+		if ($startingDateArray[1] == $endingDateArray[1]) {
+			// mois égaux
+			$startingDateFormat = '%A %d';
+		} else if ($startingDateArray[0] == $endingDateArray[0]) {
+			// annee égales
+			$startingDateFormat = '%A %d %B';
+		} else {
+			$startingDateFormat = '%A %d %B %Y';
+		}
+		
+		return Carbon::createFromFormat('Y-m-d', $startingDate)->formatLocalized($startingDateFormat).' to '.Carbon::createFromFormat('Y-m-d', $endingDate)->formatLocalized($endingDateFormat);
+
+	}
+
+}
