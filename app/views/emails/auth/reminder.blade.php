6b1e9f651eb2808687bfdb674f094c35987cb22b
Samuel De Backer <sdebacker@gmail.com>
2013-11-17 12:07:47 +0100

    routes

--- app/views/emails/auth/reminder.blade.php
+++ app/views/emails/auth/reminder.blade.php
@@ -7,7 +7,7 @@
 		<h2>Password Reset</h2>
 
 		<div>
-			To reset your password, complete this form: {{ URL::to('password/reset', array($token)) }}.
+			To reset your password, complete this form: {{ URL::route('resetpassword', array($token)) }}.
 		</div>
 	</body>
 </html>
--- app/views/emails/auth/reset.blade.php
+++ app/views/emails/auth/reset.blade.php
@@ -6,8 +6,8 @@
 	<body>
 		<h2>Password Reset</h2>
 
-		<p>To reset your password, <a href="{{ URL::to('users/changepassword', array('id' => $userId, urlencode($resetCode))) }}">click here.</a></p>
-		<p>Or point your browser to this address: <br /> {{  URL::to('users/changepassword', array('id' => $userId, urlencode($resetCode))) }}</p>
+		<p>To reset your password, <a href="{{ URL::route('changepassword', array('id' => $userId, urlencode($resetCode))) }}">click here.</a></p>
+		<p>Or point your browser to this address: <br /> {{  URL::route('changepassword', array('id' => $userId, urlencode($resetCode))) }}</p>
 		<p>Thank you, <br />
 		The Typi Team</p>
 	</body>
