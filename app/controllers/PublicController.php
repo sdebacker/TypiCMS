f50bb2177e147a6649e8ca835cb329cd7babe8a4
Samuel De Backer <sdebacker@gmail.com>
2013-11-20 20:45:02 +0100

    redirect to lang

--- app/controllers/PublicController.php
+++ app/controllers/PublicController.php
@@ -6,16 +6,27 @@ use Config;
 class PublicController extends BaseController {
 
 	/**
-	 * Fonction appelée depuis routes.php.
-	 * affiche le template publi.root (page de choix de langue)
+	 * Show lang chooser, redirect to browser lang or redirect to default lang
 	 *
 	 * @return void
 	 */
 	public function root()
 	{
+		$locales = Config::get('app.locales');
+
+
+		if (Config::get('typicms.skipLangChooser')) {
+
+			$locale = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+			! in_array($locale, $locales) and $locale = Config::get('app.locale');
+
+			return Redirect::route($locale.'.projects');
+
+		}
+
+
 		$this->title['parent'] = 'Choose your language';
 
-		$locales = Config::get('app.locales');
 		$this->layout->content = View::make('public.root')
 			->with('locales', $locales);
 	}
