a9590ed17e5d131f2c134d8e4838b582fdcdeee9
Samuel De Backer <sdebacker@gmail.com>
2013-11-22 12:09:47 +0100

    …

--- app/views/public/master.blade.php
+++ app/views/public/master.blade.php
@@ -19,7 +19,7 @@
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 	<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
+	<![endif]-->
 
 	{{ basset_javascripts('public') }}
 
