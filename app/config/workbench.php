759d85c9e68a682511d733db344a6d0f9fc6b72c
Samuel De Backer <sdebacker@gmail.com>
2013-11-22 19:58:25 +0100

    workbench config

--- app/config/workbench.php
+++ app/config/workbench.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'name' => '',
+	'name' => 'Samuel De Backer',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -26,6 +26,6 @@ return array(
 	|
 	*/
 
-	'email' => '',
+	'email' => 'sdebacker@gmail.com',
 
 );
