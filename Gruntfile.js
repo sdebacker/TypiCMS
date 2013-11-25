c8d4b2e686e316dfb4f66ef856c789cf063d4e9d
Samuel De Backer <sdebacker@gmail.com>
2013-11-26 00:17:03 +0100

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,35 @@
 module.exports = function(grunt){
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
-    });
 
-    grunt.registerTask('default', []);
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			options: {
+				separator: ';'
+			},
+			dist: {
+				src: ['src/**/*.js'],
+				dest: 'dist/<%= pkg.name %>.js'
+			}
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			},
+			dist: {
+				files: {
+					'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+				}
+			}
+		}
+		});
+
+		grunt.loadNpmTasks('grunt-contrib-uglify');
+		grunt.loadNpmTasks('grunt-contrib-cssmin');
+		grunt.loadNpmTasks('grunt-contrib-less');
+		grunt.loadNpmTasks('grunt-contrib-watch');
+		grunt.loadNpmTasks('grunt-contrib-concat');
+
+		grunt.registerTask('default', ['concat', 'uglify']);
 
 };
