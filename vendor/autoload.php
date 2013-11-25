883fe07af91711440a038a771a593b942ddfbfee
Samuel De Backer <sdebacker@gmail.com>
2013-11-25 23:56:34 +0100

    cu

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit2f5847305f8484fe417c1b29d8581622::getLoader();
+return ComposerAutoloaderInit2868765a7b0acb5f22d789fd98ea6a15::getLoader();
