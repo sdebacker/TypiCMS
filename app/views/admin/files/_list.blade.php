ff145539a987ad0ea011a94e0179584e41944480
Samuel De Backer <sdebacker@gmail.com>
2013-11-26 19:02:01 +0100

    admin thumbs

--- app/views/admin/files/_list.blade.php
+++ app/views/admin/files/_list.blade.php
@@ -1,20 +1,16 @@
 @if(count($files))
 	<h1><span id="nb_elements">{{ $files->getTotal() }}</span> @choice('global.modules.files', $files->getTotal())</h1>
 
-	<div class="row">
 	@foreach($files as $file)
-		<div class="col-md-3 col-xs-6">
-			<div class="thumbnail">
-				<img src="{{ Croppa::url('/'.$file->path.'/'.$file->filename, 135, 135) }}" alt="{{ $file->alt_attribute }}">
-				<div class="caption">
-					<p>
-						{{ $file->filename }} <br>
-						{{ $file->width }} × {{ $file->height }}
-					</p>
-				</div>
+		<div class="thumbnail">
+			<img src="{{ Croppa::url('/'.$file->path.'/'.$file->filename, 135, 135) }}" alt="{{ $file->alt_attribute }}">
+			<div class="caption">
+				<p>
+					{{ $file->filename }} <br>
+					{{ $file->width }} × {{ $file->height }}
+				</p>
 			</div>
 		</div>
 	@endforeach
-	</div>
 	
 @endif
