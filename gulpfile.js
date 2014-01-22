eb3017f51c1af5907a72b1eb3a47bdc4eb9e4688
Samuel De Backer <sdebacker@gmail.com>
2014-01-22 09:09:04 +0100

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,42 +6,48 @@ var csso = require('gulp-csso');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var watch = require('gulp-watch');
-var plumber = require('gulp-plumber');
+
+var publicJsFiles = [
+  'public/components/vendor/jquery-legacy/jquery.js',
+  'public/components/vendor/bootstrap/js/dropdown.js',
+  'public/components/vendor/fancybox/source/jquery.fancybox.js',
+  'public/js/live-search.js',
+  'public/js/gmaps.js',
+  'public/js/public.js'
+];
 
 gulp.task('default', function () {
+
+  gulp.run('public-css');
+  gulp.run('public-js');
+
+  gulp.watch('app/assets/less/*.less', function(){
+    gulp.run('public-css');
+  })
+
+  gulp.watch(publicJsFiles, function(){
+    gulp.run('public-js');
+  })
+
+});
+
+gulp.task('public-css', function () {
 
   gulp.src([
       'app/assets/less/public.less'
     ])
-    .pipe(watch())
-    .pipe(plumber())
     .pipe(less())
     .pipe(csso())
     .pipe(concat('public.css'))
     .pipe(gulp.dest('public/css'));
 
-  gulp.src([
-      'app/assets/less/admin.less'
-    ])
-    .pipe(watch())
-    .pipe(plumber())
-    .pipe(less())
-    .pipe(csso())
-    .pipe(concat('admin.css'))
-    .pipe(gulp.dest('public/css'));
+});
 
-  gulp.src([
-      'public/components/vendor/jquery-legacy/jquery.js',
-      'public/components/vendor/bootstrap/js/dropdown.js',
-      'public/components/vendor/fancybox/source/jquery.fancybox.js',
-      'public/js/live-search.js',
-      'public/js/gmaps.js',
-      'public/js/public.js'
-    ])
-    .pipe(watch())
-    .pipe(plumber())
+gulp.task('public-js', function () {
+
+  gulp.src(publicJsFiles)
     .pipe(uglify())
-    .pipe(concat('public.libs.min.js'))
+    .pipe(concat('public.min.js'))
     .pipe(gulp.dest('public/js'))
 
 });
