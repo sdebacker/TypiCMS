7e2b460fb1e22d967f79dc64563b6012d7950aff
Samuel De Backer <sdebacker@gmail.com>
2014-01-28 08:20:45 +0100

    New gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var notify = require('gulp-notify');
 var less = require('gulp-less');
 var path = require('path');
 var csso = require('gulp-csso');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var watch = require('gulp-watch');
+var exec = require('child_process').exec;
+var sys = require('sys');
 
 var publicJsFiles = [
   'public/components/vendor/jquery-legacy/jquery.js',
@@ -16,38 +19,47 @@ var publicJsFiles = [
   'public/js/public.js'
 ];
 
-gulp.task('default', function () {
+// Where do you store your Less files?
+var lessDir = 'app/assets/less';
 
-  gulp.run('public-css');
-  gulp.run('public-js');
-
-  gulp.watch('app/assets/less/*.less', function(){
-    gulp.run('public-css');
-  })
-
-  gulp.watch(publicJsFiles, function(){
-    gulp.run('public-js');
-  })
-
-});
-
+// Compile Less, concat and save to target CSS directory
 gulp.task('public-css', function () {
 
-  gulp.src([
-      'app/assets/less/public.less'
+  return gulp.src([
+      lessDir + '/public.less'
     ])
     .pipe(less())
     .pipe(csso())
     .pipe(concat('public.css'))
-    .pipe(gulp.dest('public/css'));
+    .pipe(gulp.dest('public/css'))
+    .pipe(notify('CSS minified'));
 
 });
 
+// Minify, concat and save to target JS directory
 gulp.task('public-js', function () {
 
-  gulp.src(publicJsFiles)
+  return gulp.src(publicJsFiles)
     .pipe(uglify())
     .pipe(concat('public.min.js'))
     .pipe(gulp.dest('public/js'))
+    .pipe(notify('JS minified'));
 
 });
+
+// Run all PHPUnit tests
+gulp.task('phpunit', function() {
+    exec('phpunit', function(error, stdout) {
+        sys.puts(stdout);
+    });
+});
+
+// Keep an eye on Less and PHP files for changes…
+gulp.task('watch', function () {
+    gulp.watch(lessDir + '/*.less', ['public-css']);
+    gulp.watch(publicJsFiles, ['public-js']);
+    gulp.watch('app/**/*.php', ['phpunit']);
+});
+
+// What tasks does running gulp trigger?
+gulp.task('default', ['public-css', 'public-js', 'phpunit', 'watch']);
