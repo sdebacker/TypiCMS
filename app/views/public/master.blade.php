3a5be9e790da9e9dd6f2cfd0b70d7b7008990916
Samuel De Backer <sdebacker@gmail.com>
2014-01-30 22:56:01 +0100

    respond.js

--- app/views/public/master.blade.php
+++ app/views/public/master.blade.php
@@ -21,8 +21,8 @@
 
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-	<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-	<![endif]-->
+	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
 	{{ HTML::script(asset('js/public.js')) }}
 
