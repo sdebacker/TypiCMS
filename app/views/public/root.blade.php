fd8dcd6fb29db65f2d3ec518039c924d4eeaa277
Samuel De Backer <sdebacker@gmail.com>
2014-02-03 22:51:06 +0100

    root page layout

--- app/views/public/root.blade.php
+++ app/views/public/root.blade.php
@@ -1,7 +1,7 @@
 @section('languagesMenu') @stop
 @section('header')        @stop
 @section('mainMenu')      @stop
-@section('footer')    @stop
+@section('footer')        @stop
 
 @section('main')
 
@@ -9,7 +9,11 @@
 
 		<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
 
-			<h1 class="text-center">Choose language</h1>
+			<div class="page-header">
+
+				<h1 class="text-center">Choose language</h1>
+
+			</div>
 
 
 			<div class="btn-group btn-group-justified">
