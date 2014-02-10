ed9f23c41b1c218abe38b897ffed679003c0ec19
Samuel De Backer <sdebacker@gmail.com>
2014-02-10 17:47:02 +0100

    menulinks form

--- app/TypiCMS/Modules/Menulinks/Views/menulinks/admin/_form.blade.php
+++ app/TypiCMS/Modules/Menulinks/Views/menulinks/admin/_form.blade.php
@@ -1,12 +1,12 @@
 <div class="row">
 
 	<div class="form-group col-sm-12">
-		{{ Former::primary_button()->type('submit')->value('save') }}
-		{{ Former::primary_button()->type('submit')->name('exit')->setAttribute('value', 'true')->value('save and exit') }}
-		{{ Former::link()->class('btn btn-default')->href(route('admin.menus.menulinks.index', $menu->id))->value('Annuler') }}
+		<button class="btn-primary btn" type="submit">@lang('validation.attributes.save')</button>
+		<button class="btn-primary btn" value="true" id="exit" name="exit" type="submit">@lang('validation.attributes.save and exit')</button>
+		<a href="{{ route('admin.menus.menulinks.index', $menu->id) }}" class="btn btn-default">@lang('validation.attributes.exit')</a>
 	</div>
 
-	{{ Former::hidden('id'); }}
+	{{ Form::hidden('id'); }}
 
 	<div class="col-sm-6">
 
@@ -17,10 +17,23 @@
 			@foreach ($locales as $lang)
 
 			<div class="tab-pane fade @if ($locale == $lang)in active@endif" id="{{ $lang }}">
-				{{ Former::lg_text($lang.'[title]')->label('title'); }}
-				{{ Former::text($lang.'[uri]')->label('uri'); }}
-				{{ Former::text($lang.'[url]')->label('website'); }}
-				{{ Former::checkbox($lang.'[status]')->label('')->text('Online'); }}
+				<div class="form-group">
+					{{ Form::label($lang.'[title]', trans('validation.attributes.title')) }}
+					{{ Form::text($lang.'[title]', $model->$lang->title, array('autofocus' => 'autofocus', 'class' => 'input-lg form-control')) }}
+				</div>
+				<div class="form-group">
+					{{ Form::label($lang.'[uri]', trans('validation.attributes.uri')) }}
+					{{ Form::text($lang.'[uri]', $model->$lang->uri, array('class' => 'form-control')) }}
+				</div>
+				<div class="form-group">
+					{{ Form::label($lang.'[url]', trans('validation.attributes.website')) }}
+					{{ Form::text($lang.'[url]', $model->$lang->url, array('class' => 'form-control')) }}
+				</div>
+				<div class="form-group">
+					<label class="checkbox">
+						{{ Form::checkbox($lang.'[status]', 1, $model->$lang->status) }} @lang('validation.attributes.online')
+					</label>
+				</div>
 			</div>
 
 			@endforeach
@@ -31,26 +44,44 @@
 
 	<div class="col-sm-6">
 
-		<div class="row">
-			<div class="col-sm-6">{{ Former::select('page_id')->options($selectPages)->small(6); }}</div>
-			<div class="col-sm-6">{{ Former::select('module_name')->options($selectModules); }}</div>
+		<div class="row form-group">
+			<div class="col-sm-6">
+				{{ Form::label('page_id', trans('validation.attributes.page_id')) }}
+				{{ Form::select('page_id', $selectPages, null, array('class' => 'form-control')) }}
+			</div>
+			<div class="col-sm-6">
+				{{ Form::label('module_name', trans('validation.attributes.module_name')) }}
+				{{ Form::select('module_name', $selectModules, null, array('class' => 'form-control')) }}
+			</div>
 		</div>
 
-		<div class="row">
-			<div class="col-sm-6">{{ Former::select('target')->options(array('' => trans('validation.values.Active tab'), '_blank' => trans('validation.values.New tab'))); }}</div>
-			<div class="col-sm-6">{{ Former::text('class'); }}</div>
+		<div class="row form-group">
+			<div class="col-sm-6">
+				{{ Form::label('target', trans('validation.attributes.target')) }}
+				{{ Form::select('target', array('' => trans('validation.values.Active tab'), '_blank' => trans('validation.values.New tab')), null, array('class' => 'form-control')) }}
+			</div>
+			<div class="col-sm-6">
+				{{ Form::label('class', trans('validation.attributes.class')) }}
+				{{ Form::text('class', $model->$lang->class, array('class' => 'form-control')) }}
+			</div>
 		</div>
 		
-		<div class="row">
-			<div class="col-sm-6">{{ Former::text('restricted_to'); }}</div>
-			<div class="col-sm-6">{{ Former::text('link_type'); }}</div>
+		<div class="row form-group">
+			<div class="col-sm-6">
+				{{ Form::label('restricted_to', trans('validation.attributes.restricted_to')) }}
+				{{ Form::text('restricted_to', $model->$lang->restricted_to, array('class' => 'form-control')) }}
+			</div>
+			<div class="col-sm-6">
+				{{ Form::label('link_type', trans('validation.attributes.link_type')) }}
+				{{ Form::text('link_type', $model->$lang->link_type, array('class' => 'form-control')) }}
+			</div>
 		</div>
 
 	</div>
 
 </div>
 
-{{ Former::hidden('menu_id', $menu->id); }}
-{{ Former::hidden('position'); }}
-{{ Former::hidden('parent'); }}
-{{ Former::hidden('id'); }}
+{{ Form::hidden('menu_id', $menu->id); }}
+{{ Form::hidden('position'); }}
+{{ Form::hidden('parent'); }}
+{{ Form::hidden('id'); }}
--- app/TypiCMS/Modules/Menulinks/Views/menulinks/admin/edit.blade.php
+++ app/TypiCMS/Modules/Menulinks/Views/menulinks/admin/edit.blade.php
@@ -1,6 +1,6 @@
 @section('main')
 
-	{{ Form::model( $model, array( 'route' => array('admin.menus.menulinks.update', array($model->menu_id, $model->id)), 'method' => 'patch', 'role' => 'form' ) ) }}
+	{{ Form::model( $model, array( 'route' => array('admin.menus.menulinks.update', $model->menu_id, $model->id), 'method' => 'patch', 'role' => 'form' ) ) }}
 		@include('menulinks.admin._form')
 	{{ Form::close() }}
 
