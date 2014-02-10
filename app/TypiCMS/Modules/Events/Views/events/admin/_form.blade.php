2c07ec8ef03491702c0e6ff6b167a0a48c62620e
Samuel De Backer <sdebacker@gmail.com>
2014-02-10 17:58:52 +0100

    events form

--- app/TypiCMS/Modules/Events/Views/events/admin/_form.blade.php
+++ app/TypiCMS/Modules/Events/Views/events/admin/_form.blade.php
@@ -10,23 +10,39 @@
 
 	{{ Form::hidden('id'); }}
 
-	<div class="col-sm-12">
+	<div class="col-sm-6">
 
-		<div class="row">
+		<div class="row form-group">
 			<div class="col-xs-8">
-				{{ Former::text('start_date')->placeholder('DDMMYYYY')->class('form-control datepicker datepicker-start')->append('<span class="glyphicon glyphicon-calendar"></span>'); }}
+				{{ Form::label('start_date', trans('validation.attributes.start_date'), array('class' => 'control-label', 'placeholder' => 'DDMMYYYY')) }}
+				<div class="input-group">
+					{{ Form::text('start_date', null, array('class' => 'form-control datepicker datepicker-start')) }}
+					<span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
+				</div>
 			</div>
 			<div class="col-xs-4">
-				{{ Former::text('start_time')->placeholder('HH:MM')->class('form-control timepicker')->append('<span class="glyphicon glyphicon-time"></span>'); }}
+				{{ Form::label('start_time', trans('validation.attributes.start_time'), array('class' => 'control-label', 'placeholder' => 'HH:MM')) }}
+				<div class="input-group">
+					{{ Form::text('start_time', null, array('class' => 'form-control timepicker')) }}
+					<span class="input-group-addon"><i class="glyphicon glyphicon-time"></i></span>
+				</div>
 			</div>
 		</div>
 
-		<div class="row">
+		<div class="row form-group">
 			<div class="col-xs-8">
-				{{ Former::text('end_date')->placeholder('DDMMYYYY')->class('form-control datepicker datepicker-end')->append('<span class="glyphicon glyphicon-calendar"></span>'); }}
+				{{ Form::label('end_date', trans('validation.attributes.end_date'), array('class' => 'control-label', 'placeholder' => 'DDMMYYYY')) }}
+				<div class="input-group">
+					{{ Form::text('end_date', null, array('class' => 'form-control datepicker datepicker-end')) }}
+					<span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
+				</div>
 			</div>
 			<div class="col-xs-4">
-				{{ Former::text('end_time')->placeholder('HH:MM')->class('form-control timepicker')->append('<span class="glyphicon glyphicon-time"></span>'); }}
+				{{ Form::label('end_time', trans('validation.attributes.end_time'), array('class' => 'control-label', 'placeholder' => 'HH:MM')) }}
+				<div class="input-group">
+					{{ Form::text('end_time', null, array('class' => 'form-control timepicker')) }}
+					<span class="input-group-addon"><i class="glyphicon glyphicon-time"></i></span>
+				</div>
 			</div>
 		</div>
 
@@ -37,17 +53,40 @@
 			@foreach ($locales as $lang)
 
 			<div class="tab-pane fade @if ($locale == $lang)in active@endif" id="{{ $lang }}">
-				{{ Former::lg_text($lang.'[title]')->label('title'); }}
-				{{ Former::text($lang.'[slug]')->label('slug'); }}
-				{{ Former::checkbox($lang.'[status]')->text('Online')->label(''); }}
-				{{ Former::textarea($lang.'[summary]')->label('summary')->value(''); }}
-				{{ Former::textarea($lang.'[body]')->label('body')->class('form-control editor')->value(''); }}
+				<div class="form-group">
+					{{ Form::label($lang.'[title]', trans('validation.attributes.title')) }}
+					{{ Form::text($lang.'[title]', $model->$lang->title, array('autofocus' => 'autofocus', 'class' => 'input-lg form-control')) }}
+				</div>
+				<div class="form-group @if($errors->has($lang.'.slug'))has-error@endif">
+					{{ Form::label($lang.'[slug]', trans('validation.attributes.slug'), array('class' => 'control-label')) }}
+					{{ Form::text($lang.'[slug]', $model->$lang->slug, array('class' => 'form-control')) }}
+					@if($errors->has($lang.'.slug'))
+					<span class="help-block">{{ $errors->first($lang.'.slug') }}</span>
+					@endif
+				</div>
+				<div class="form-group">
+					<label class="checkbox">
+						{{ Form::checkbox($lang.'[status]', 1, $model->$lang->status) }} @lang('validation.attributes.online')
+					</label>
+				</div>
+				<div class="form-group">
+					{{ Form::label($lang.'[summary]', trans('validation.attributes.summary')) }}
+					{{ Form::textarea($lang.'[summary]', $model->$lang->summary, array('class' => 'form-control', 'rows' => 4)) }}
+				</div>
+				<div class="form-group">
+					{{ Form::label($lang.'[body]', trans('validation.attributes.body')) }}
+					{{ Form::textarea($lang.'[body]', $model->$lang->body, array('class' => 'editor form-control')) }}
+				</div>
 			</div>
 
 			@endforeach
 
 		</div>
 
+	</div>
+
+	<div class="col-sm-6">
+	@include('files.admin._list', array('files' => $model->files))
 	</div>
 
 </div>
--- app/TypiCMS/Modules/Events/Views/events/admin/edit.blade.php
+++ app/TypiCMS/Modules/Events/Views/events/admin/edit.blade.php
@@ -1,15 +1,7 @@
 @section('main')
 
-	<div class="row">
-
-		{{ Form::model( $model, array( 'route' => array('admin.events.update', $model->id), 'method' => 'patch', 'role' => 'form' ) ) }}
-			@include('events.admin._form')
-		{{ Form::close() }}
-
-		<div class="col-sm-6">
-		@include('files.admin._list', array('files' => $model->files))
-		</div>
-
-	</div>
+	{{ Form::model( $model, array( 'route' => array('admin.events.update', $model->id), 'method' => 'patch', 'role' => 'form' ) ) }}
+		@include('events.admin._form')
+	{{ Form::close() }}
 
 @stop
