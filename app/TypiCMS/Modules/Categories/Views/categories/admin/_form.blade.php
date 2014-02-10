053441f2504e15a21298913aa65164eeab85c996
Samuel De Backer <sdebacker@gmail.com>
2014-02-10 17:17:22 +0100

    Categories form

--- app/TypiCMS/Modules/Categories/Views/categories/admin/_form.blade.php
+++ app/TypiCMS/Modules/Categories/Views/categories/admin/_form.blade.php
@@ -8,7 +8,7 @@
 
 	@include('admin._buttons-form')
 
-	{{ Former::hidden('id'); }}
+	{{ Form::hidden('id'); }}
 
 	<div class="col-sm-6">
 
@@ -19,9 +19,22 @@
 			@foreach ($locales as $lang)
 
 			<div class="tab-pane fade @if ($locale == $lang)in active@endif" id="{{ $lang }}">
-				{{ Former::lg_text($lang.'[title]')->label('title'); }}
-				{{ Former::text($lang.'[slug]')->label('slug'); }}
-				{{ Former::checkbox($lang.'[status]')->text('Online')->label(''); }}
+				<div class="form-group">
+					{{ Form::label($lang.'[title]', trans('validation.attributes.title')) }}
+					{{ Form::text($lang.'[title]', $model->$lang->title, array('autofocus' => 'autofocus', 'class' => 'input-lg form-control')) }}
+				</div>
+				<div class="form-group @if($errors->has($lang.'.slug'))has-error@endif">
+					{{ Form::label($lang.'[slug]', trans('validation.attributes.slug')) }}
+					{{ Form::text($lang.'[slug]', $model->$lang->slug, array('class' => 'form-control')) }}
+					@if($errors->has($lang.'.slug'))
+					<span class="help-block">{{ $errors->first($lang.'.slug') }}</span>
+					@endif
+				</div>
+				<div class="form-group">
+					<label class="checkbox">
+						{{ Form::checkbox($lang.'[status]', 1, $model->$lang->status) }} @lang('validation.attributes.online')
+					</label>
+				</div>
 			</div>
 
 			@endforeach
