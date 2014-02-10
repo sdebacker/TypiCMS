f54ffc60446136c062e0631c0fa9b6e23b5cad5d
Samuel De Backer <sdebacker@gmail.com>
2014-02-10 16:53:54 +0100

    form buttons

--- app/views/admin/_buttons-form.blade.php
+++ app/views/admin/_buttons-form.blade.php
@@ -1,5 +1,5 @@
 	<div class="form-group col-sm-12">
-		{{ Former::primary_button()->type('submit')->value('save') }}
-		{{ Former::primary_button()->type('submit')->name('exit')->setAttribute('value', 'true')->value('save and exit') }}
-		{{ Former::link()->class('btn btn-default')->href(route('admin.' . $model->view . '.index'))->value('Annuler') }}
+		<button class="btn-primary btn" type="submit">@lang('validation.attributes.save')</button>
+		<button class="btn-primary btn" value="true" id="exit" name="exit" type="submit">@lang('validation.attributes.save and exit')</button>
+		<a href="{{ route('admin.' . $model->view . '.index') }}" class="btn btn-default">@lang('validation.attributes.exit')</a>
 	</div>
