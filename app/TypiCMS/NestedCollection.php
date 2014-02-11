ca08eda54e9e0e77eb60b55e391d2f1d2e80502e
Samuel De Backer <sdebacker@gmail.com>
2014-02-11 15:49:37 +0100

    nestedCollection cleaned

--- app/TypiCMS/NestedCollection.php
+++ app/TypiCMS/NestedCollection.php
@@ -1,12 +1,10 @@
 <?php namespace TypiCMS;
 
 use Illuminate\Database\Eloquent\Collection;
-use TypiCMS\Services\ListBuilder\ListBuilder;
 
 class NestedCollection extends Collection {
 
 	private $total = 0;
-	protected $list;
 
 	public function __construct(array $items = array())
 	{
@@ -22,51 +20,27 @@ class NestedCollection extends Collection {
 	 */
 	public function nest()
 	{
-		// set keys to id
-		$tmpArray = array();
-		foreach ($this->items as $item1) {
-			$tmpArray[$item1->id] = $item1;
-		}
-		$this->items = $tmpArray;
+		// Set id as keys
+		$this->items = $this->getDictionary($this);
 
+		// Set children
 		$deleteArray = array();
-		// set children
-		foreach ($this->items as $item2) {
-			if ( $item2->parent && isset($this->items[$item2->parent]) ) {
-				$this->items[$item2->parent]->children[] = $item2;
-				$deleteArray[] = $item2->id;
+		foreach ($this->items as $item) {
+			if ( $item->parent && isset($this->items[$item->parent]) ) {
+				$this->items[$item->parent]->children[] = $item;
+				$deleteArray[] = $item->id;
 			}
 		}
 
-		// delete moved items
-		foreach ($deleteArray as $id) {
-			unset($this->items[$id]);
-		}
-		
+		// Delete moved items
+		$this->items = array_except($this->items, $deleteArray);
+
 		return $this;
 	}
 
 
 	/**
-	 * Build html list
-	 *
-	 * @param array
-     * @return string
-	 */
-	public function buildList($properties = array())
-	{
-		$this->list = with(new ListBuilder(null, $properties))->build($this->items);
-		return $this;
-	}
-
-
-	public function getList()
-	{
-		return $this->list;
-	}
-
-	/**
-	 * Get total items in collection
+	 * Get total items in nested collection
 	 *
 	 * @return int
 	 */
