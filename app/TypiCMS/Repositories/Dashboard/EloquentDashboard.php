a3ed55503ddf4bc584df09b8be960ca9df7b13b8
Samuel De Backer <sdebacker@gmail.com>
2014-02-17 20:45:23 +0100

    bug

--- app/TypiCMS/Repositories/Dashboard/EloquentDashboard.php
+++ app/TypiCMS/Repositories/Dashboard/EloquentDashboard.php
@@ -47,7 +47,7 @@ class EloquentDashboard extends RepositoriesAbstract implements DashboardInterfa
 			if ($property['dashboard'] and Sentry::getUser()->hasAccess('admin.' . $lowerName . '.index')) {
 				$modulesForDashboard[$module]['name'] = $module;
 				$modulesForDashboard[$module]['route'] = $lowerName;
-				$modulesForDashboard[$module]['title'] = Str::title(trans_choice('modules.'.$lowerName.'.'.$lowerName), 2));
+				$modulesForDashboard[$module]['title'] = Str::title(trans_choice('modules.'.$lowerName.'.'.$lowerName, 2));
 				$modulesForDashboard[$module]['count'] = DB::table($lowerName)->count();
 			}
 		}
