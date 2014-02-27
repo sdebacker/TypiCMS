a290f1200ce1fff7c04fb19957e1d3bbf1648c16
Samuel De Backer <sdebacker@gmail.com>
2014-02-27 10:03:22 +0100

    red tab on validation error

--- public/js/form.js
+++ public/js/form.js
@@ -47,6 +47,18 @@ function initTinymce(selector) {
 			})
 		};
 
+		// Set tab in red on validation errors
+		var firstErrorTabActive = false;
+		$('.tab-pane').each(function(index, el) {
+			if ($(this).find('.has-error').length) {
+				if ( ! firstErrorTabActive) {
+					$('a[href="#' + $(this).attr('id') + '"]').tab('show');
+					firstErrorTabActive = true;
+				}
+				$('a[href="#' + $(this).attr('id') + '"]').addClass('text-danger');
+			};
+		});
+
 		if ($('.datepicker').length) {
 			$('.datepicker').parent().datetimepicker({
 				format: 'DD.MM.YYYY',
