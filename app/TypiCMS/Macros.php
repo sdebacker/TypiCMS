32cf1f5fc50176402f2e8aa8b01fe50c155147ef
Samuel De Backer <sdebacker@gmail.com>
2014-03-04 11:43:30 +0100

    tableBuilder removed

--- app/TypiCMS/Macros.php
+++ app/TypiCMS/Macros.php
@@ -4,11 +4,6 @@ HTML::macro('adminList', function($items = array(), $properties = array())
 	return with(new TypiCMS\Services\ListBuilder\ListBuilder(null, $properties))->build($items);
 });
 
-HTML::macro('adminTable', function($items = array(), $properties = array())
-{
-	return with(new TypiCMS\Services\TableBuilder\TableBuilder(null, $properties))->build($items);
-});
-
 HTML::macro('th', function($field = '', $sortable = true)
 {
 	return with(new TypiCMS\Services\Html)->th($field, $sortable);
