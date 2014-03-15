30aab4292473f5b2ba334371f7e989a551ea6d78
Samuel De Backer <sdebacker@gmail.com>
2014-03-15 11:09:38 +0100

    macro

--- app/TypiCMS/Macros.php
+++ app/TypiCMS/Macros.php
@@ -1,5 +1,31 @@
 <?php 
 HTML::macro('th', function($field = '', $defaultOrder = null, $sortable = true, $label = true)
 {
-	return with(new TypiCMS\Services\Html)->th($field, $defaultOrder, $sortable, $label);
+	$order = Input::get('order');
+	if ( ! $order and $defaultOrder) {
+		$order = $field;
+	}
+	$direction = Input::get('direction', $defaultOrder);
+	$th[] = '<th class="' . $field . '">';
+	if ($sortable) {
+		$newDirection = 'asc';
+		$iconDir = ' text-muted';
+		if ($order == $field) {
+			if ($direction == 'asc') {
+				$newDirection = 'desc';
+			}
+			$iconDir = '-' . $newDirection;
+		}
+		$th[] = '<a href="?order=' . $field . '&direction=' . $newDirection . '">';
+		$th[] = '<i class="fa fa-sort' . $iconDir . '"></i> ';
+	}
+	if ($label) {
+		$th[] = trans('validation.attributes.' . $field);
+	}
+	if ($sortable) {
+		$th[] = '</a>';
+	}
+	$th[] = '</th>';
+	$th[] = "\r\n";
+	return implode($th);
 });
