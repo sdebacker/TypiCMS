cc7a8bd93bb3da3088ba0d50f7f4d40d44b5dc86
Samuel De Backer <sdebacker@gmail.com>
2014-03-16 22:34:03 +0100

    local env

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('Ordi-de-Samuel.local'),
+	'local' => array('*.local', '*.dev', '*.development'),
 
 ));
 
