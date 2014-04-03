2124b38b6451800315f84c486988a4fa5dbec76c
Samuel De Backer <sdebacker@gmail.com>
2014-04-03 09:26:51 +0200

    events seeding

--- app/database/seeds/EventSeeder.php
+++ app/database/seeds/EventSeeder.php
@@ -13,18 +13,18 @@ class EventSeeder extends Seeder
         );
 
         $typi_event_translations = array(
-            array('id' => '1','event_id' => '1','locale' => 'fr','status' => '1','title' => 'Concert à la maison','slug' => 'concert-a-la-maison','summary' => '','body' => '','created_at' => '2013-10-28 23:20:14','updated_at' => '2013-10-28 23:20:14'),
+            array('id' => '1','event_id' => '1','locale' => 'fr','status' => '1','title' => 'Evenement 4','slug' => 'evenement-4','summary' => '','body' => '','created_at' => '2013-10-28 23:20:14','updated_at' => '2013-10-28 23:20:14'),
             array('id' => '2','event_id' => '1','locale' => 'nl','status' => '0','title' => '','slug' => NULL,'summary' => '','body' => '','created_at' => '2013-10-28 23:20:14','updated_at' => '2013-10-28 23:20:14'),
             array('id' => '3','event_id' => '1','locale' => 'en','status' => '0','title' => '','slug' => NULL,'summary' => '','body' => '','created_at' => '2013-10-28 23:20:14','updated_at' => '2013-10-28 23:20:14'),
-            array('id' => '4','event_id' => '2','locale' => 'fr','status' => '1','title' => 'Cabaret Boite à Clous','slug' => 'cabaret-boite-a-clous','summary' => '','body' => '','created_at' => '2013-10-28 23:21:10','updated_at' => '2013-10-28 23:21:10'),
-            array('id' => '5','event_id' => '2','locale' => 'nl','status' => '1','title' => 'Boite à Clous Cabaret','slug' => 'boite-a-clous-cabaret','summary' => '','body' => '','created_at' => '2013-10-28 23:21:10','updated_at' => '2013-10-28 23:21:10'),
+            array('id' => '4','event_id' => '2','locale' => 'fr','status' => '1','title' => 'Evenement 1','slug' => 'evenement-1','summary' => '','body' => '','created_at' => '2013-10-28 23:21:10','updated_at' => '2013-10-28 23:21:10'),
+            array('id' => '5','event_id' => '2','locale' => 'nl','status' => '1','title' => 'Event 1','slug' => 'event-1','summary' => '','body' => '','created_at' => '2013-10-28 23:21:10','updated_at' => '2013-10-28 23:21:10'),
             array('id' => '6','event_id' => '2','locale' => 'en','status' => '0','title' => '','slug' => NULL,'summary' => '','body' => '','created_at' => '2013-10-28 23:21:10','updated_at' => '2013-10-28 23:21:10'),
-            array('id' => '7','event_id' => '3','locale' => 'fr','status' => '1','title' => 'Vacances d’hiver','slug' => 'vacances-dhiver','summary' => '','body' => '','created_at' => '2013-10-28 23:22:37','updated_at' => '2013-10-28 23:22:37'),
-            array('id' => '8','event_id' => '3','locale' => 'nl','status' => '0','title' => 'Vacancies','slug' => 'vacancies','summary' => '','body' => '','created_at' => '2013-10-28 23:22:37','updated_at' => '2013-10-28 23:22:37'),
-            array('id' => '9','event_id' => '3','locale' => 'en','status' => '1','title' => 'Holidays','slug' => 'holidays','summary' => '','body' => '','created_at' => '2013-10-28 23:22:37','updated_at' => '2013-10-28 23:22:37'),
-            array('id' => '10','event_id' => '4','locale' => 'fr','status' => '1','title' => 'Concert','slug' => 'concert','summary' => '','body' => '','created_at' => '2014-03-28 23:22:37','updated_at' => '2014-03-28 23:22:37'),
-            array('id' => '11','event_id' => '4','locale' => 'nl','status' => '0','title' => 'Concert','slug' => 'concert','summary' => '','body' => '','created_at' => '2014-03-28 23:22:37','updated_at' => '2014-03-28 23:22:37'),
-            array('id' => '12','event_id' => '4','locale' => 'en','status' => '1','title' => 'Concert','slug' => 'concert','summary' => '','body' => '','created_at' => '2014-03-28 23:22:37','updated_at' => '2014-03-28 23:22:37')
+            array('id' => '7','event_id' => '3','locale' => 'fr','status' => '1','title' => 'Evenement 2','slug' => 'evenement-2','summary' => '','body' => '','created_at' => '2013-10-28 23:22:37','updated_at' => '2013-10-28 23:22:37'),
+            array('id' => '8','event_id' => '3','locale' => 'nl','status' => '0','title' => 'Event 2','slug' => 'event-2','summary' => '','body' => '','created_at' => '2013-10-28 23:22:37','updated_at' => '2013-10-28 23:22:37'),
+            array('id' => '9','event_id' => '3','locale' => 'en','status' => '1','title' => 'Event 2','slug' => 'event-2','summary' => '','body' => '','created_at' => '2013-10-28 23:22:37','updated_at' => '2013-10-28 23:22:37'),
+            array('id' => '10','event_id' => '4','locale' => 'fr','status' => '1','title' => 'Evenement 3','slug' => 'evenement-3','summary' => '','body' => '','created_at' => '2014-03-28 23:22:37','updated_at' => '2014-03-28 23:22:37'),
+            array('id' => '11','event_id' => '4','locale' => 'nl','status' => '0','title' => 'Event 3','slug' => 'event-3','summary' => '','body' => '','created_at' => '2014-03-28 23:22:37','updated_at' => '2014-03-28 23:22:37'),
+            array('id' => '12','event_id' => '4','locale' => 'en','status' => '1','title' => 'Event 3','slug' => 'event-3','summary' => '','body' => '','created_at' => '2014-03-28 23:22:37','updated_at' => '2014-03-28 23:22:37')
         );
 
         DB::table('events')->insert( $typi_events );
