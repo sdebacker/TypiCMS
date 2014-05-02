369b184f21488856e6dede430d9fda1c632b853d
Samuel De Backer <sdebacker@gmail.com>
2014-05-02 23:32:24 +0200

    SlugObserver Class

---
+++ app/TypiCMS/Observers/SlugObserver.php
@@ -0,0 +1,40 @@
+<?php
+namespace TypiCMS\Observers;
+
+class SlugObserver
+{
+
+    public function creating($model)
+    {
+        // slug = null si vide
+        $slug = ($model->slug) ? $model->slug : null ;
+        $model->slug = $slug;
+
+        if ($slug) {
+            $i = 0;
+            // Check uri is unique
+            while ($model::where('slug', $model->slug)->where('locale', $model->locale)->first()) {
+                $i++;
+                // increment slug if exists
+                $model->slug = $slug.'-'.$i;
+            }
+        }
+    }
+
+    public function updating($model)
+    {
+        // slug = null si vide
+        $slug = ($model->slug) ? $model->slug : null ;
+        $model->slug = $slug;
+
+        if ($slug) {
+            $i = 0;
+            // Check uri is unique
+            while ($model::where('slug', $model->slug)->where('id', '!=', $model->id)->where('locale', $model->locale)->first()) {
+                $i++;
+                // increment slug if exists
+                $model->slug = $slug.'-'.$i;
+            }
+        }
+    }
+}
