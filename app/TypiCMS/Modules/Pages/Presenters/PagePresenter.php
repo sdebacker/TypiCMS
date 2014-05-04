e12f71e1934dcd3fdd4c7fd17b30a18e1d6202a7
Samuel De Backer <sdebacker@gmail.com>
2014-05-04 23:19:11 +0200

    Cannot delete homepage

--- app/TypiCMS/Modules/Pages/Presenters/PagePresenter.php
+++ app/TypiCMS/Modules/Pages/Presenters/PagePresenter.php
@@ -5,8 +5,26 @@ use TypiCMS\Presenters\Presenter;
 
 class PagePresenter extends Presenter
 {
+    /**
+     * Get public url
+     * @param  string $lang
+     * @return string       uri
+     */
     public function publicUri($lang)
     {
         return '/' . $this->entity->$lang->uri;
+    }
+
+    /**
+    * Checkboxes
+    *
+    * @return string
+    */
+    public function checkbox()
+    {
+        // Disable checkbox when object has menulinks
+        $disabled = $this->entity->is_home ? ' disabled' : '' ;
+
+        return '<input type="checkbox" value="' . $this->entity->id . '"' . $disabled . '>';
     }
 }
