88c8dfcbd447d5d419ae07dec4f5b946484d4496
Samuel De Backer <sdebacker@gmail.com>
2014-05-08 16:32:01 +0200

    test mail

--- app/config/mail.php
+++ app/config/mail.php
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'admin@example.com', 'name' => null),
 
 	/*
 	|--------------------------------------------------------------------------
