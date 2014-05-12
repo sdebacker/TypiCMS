a2456e6e21be59b5c7b94489e51200effd5dd324
Samuel De Backer <sdebacker@gmail.com>
2014-05-12 17:11:50 +0200

    no lang buttons above lists if only one locale

--- app/views/admin/_buttons-list.blade.php
+++ app/views/admin/_buttons-list.blade.php
@@ -1,9 +1,11 @@
         <div class="btn-toolbar" role="toolbar">
             <div class="btn-group pull-right">
                 @section('btn-locales')
-                    @foreach (Config::get('app.locales') as $locale)
-                        {{ Html::langButton($locale) }}
-                    @endforeach
+                    @if (count(Config::get('app.locales')) > 1)
+                        @foreach (Config::get('app.locales') as $locale)
+                            {{ Html::langButton($locale) }}
+                        @endforeach
+                    @endif
                 @show
             </div>
         </div>
