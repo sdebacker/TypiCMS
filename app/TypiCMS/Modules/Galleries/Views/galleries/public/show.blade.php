10f72c1b44cc173a7e5062ee213bd882c28902c6
Samuel De Backer <sdebacker@gmail.com>
2014-05-20 23:05:32 +0200

    Images galleries on public side

--- app/TypiCMS/Modules/Galleries/Views/galleries/public/show.blade.php
+++ app/TypiCMS/Modules/Galleries/Views/galleries/public/show.blade.php
@@ -3,11 +3,19 @@
 
     <article>
         <h2>{{ $model->title }}</h2>
-        <div class="date">{{ $model->present()->dateFromTo }}</div>
-        <p>Heure début : {{ $model->start_time }}</p>
-        <p>Heure fin : {{ $model->end_time }}</p>
-        <p class="summary">{{ nl2br($model->summary) }}</p>
+        <p class="lead summary">{{ nl2br($model->summary) }}</p>
         <div>{{ $model->body }}</div>
     </article>
+    <div class="row">
+    @foreach ($model->files as $image)
+        <div class="col-xs-4 col-sm-3 col-md-2">
+            <div class="thumbnail">
+                <a class="fancybox" href="/uploads/{{ $image->filename }}" data-fancybox-group="{{ $model->slug }}">
+                    <img class="img-responsive" src="{{ Croppa::url('/uploads/' . $image->filename, 200, 200) }}" alt="">
+                </a>
+            </div>
+        </div>
+    @endforeach
+    </div>
 
 @stop
