d8e79d077dd76fcfd1adc616463aba065b61e0de
Samuel De Backer <sdebacker@gmail.com>
2014-05-20 23:34:02 +0200

    galleries : no div.row when no images

--- app/TypiCMS/Modules/Galleries/Views/galleries/public/show.blade.php
+++ app/TypiCMS/Modules/Galleries/Views/galleries/public/show.blade.php
@@ -6,16 +6,18 @@
         <p class="lead summary">{{ nl2br($model->summary) }}</p>
         <div>{{ $model->body }}</div>
     </article>
-    <div class="row">
-    @foreach ($model->files as $image)
-        <div class="col-xs-4 col-sm-3 col-md-2">
-            <div class="thumbnail">
-                <a class="fancybox" href="/uploads/{{ $image->filename }}" data-fancybox-group="{{ $model->slug }}">
-                    <img class="img-responsive" src="{{ Croppa::url('/uploads/' . $image->filename, 200, 200) }}" alt="">
-                </a>
+    @if ($model->files->count())
+        <div class="row">
+        @foreach ($model->files as $image)
+            <div class="col-xs-4 col-sm-3 col-md-2">
+                <div class="thumbnail">
+                    <a class="fancybox" href="/uploads/{{ $image->filename }}" data-fancybox-group="{{ $model->slug }}">
+                        <img class="img-responsive" src="{{ Croppa::url('/uploads/' . $image->filename, 200, 200) }}" alt="">
+                    </a>
+                </div>
             </div>
+        @endforeach
         </div>
-    @endforeach
-    </div>
+    @endif
 
 @stop
