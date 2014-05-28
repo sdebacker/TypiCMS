57bab730a5b754861f27277b149643ead6a70ecb
Samuel De Backer <sdebacker@gmail.com>
2014-05-29 00:16:21 +0200

    …

---
+++ app/TypiCMS/Modules/News/Views/news/public/show.blade.php
@@ -0,0 +1,15 @@
+
+@section('main')
+
+    <article>
+        <h2>{{ $model->title }}</h2>
+        <div class="date">@lang('news::global.Published on') 
+            <time datetime="{{ $model->date }}" pubdate>{{ $model->present()->dateLocalized }}</time>
+        </div>
+        <p class="summary">{{ nl2br($model->summary) }}</p>
+        <div class="body">{{ $model->body }}</div>
+    </article>
+
+    @include('public._galleries')
+
+@stop
