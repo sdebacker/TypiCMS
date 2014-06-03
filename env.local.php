52e665b4c683557c9edfb6cb6f69c71bf5129496
Samuel De Backer <sdebacker@gmail.com>
2014-06-03 23:42:56 +0200

    env file

--- env.local.php
+++ env.local.php
@@ -3,4 +3,21 @@ return array(
 	'MYSQL_DATABASE' => 'typicms',
 	'MYSQL_USERNAME' => 'root',
 	'MYSQL_PASSWORD' => '',
+
+    'REMOTE_HOST'      => '',
+    'REMOTE_USERNAME'  => '',
+    'REMOTE_PASSWORD'  => '',
+    'REMOTE_KEY'       => '',
+    'REMOTE_KEYPHRASE' => '',
+    'REMOTE_ROOT'      => '',
+
+    'MAILGUN_DOMAIN'   => '',
+    'MAILGUN_SECRET'   => '',
+
+    'MANDRILL_SECRET'  => '',
+
+    'STRIPE_SECRET'    => '',
+
+    'AWS_KEY'          => '',
+    'AWS_SECRET'       => '',
 );
