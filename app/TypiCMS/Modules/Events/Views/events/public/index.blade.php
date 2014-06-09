099724907a404eca5453fc7e44f8531f374eb6f0
Samuel De Backer <sdebacker@gmail.com>
2014-06-09 20:51:37 +0200

    missing <br>

--- app/TypiCMS/Modules/Events/Views/events/public/index.blade.php
+++ app/TypiCMS/Modules/Events/Views/events/public/index.blade.php
@@ -7,7 +7,7 @@
         @foreach ($models as $model)
         <li>
             <strong>{{ $model->title }}</strong>
-            <div class="date">{{ $model->present()->dateFromTo }} {{ $model->present()->timeFromTo }}</div>
+            <div class="date">{{ $model->present()->dateFromTo }} <br>{{ $model->present()->timeFromTo }}</div>
             <a href="{{ route($lang.'.events.slug', $model->slug) }}">@lang('db.More')</a>
             <a class="btn btn-default btn-xs" href="{{ route($lang.'.events.slug.ics', $model->slug) }}">
                 <span class="fa fa-calendar"></span> @lang('db.Add to calendar')
