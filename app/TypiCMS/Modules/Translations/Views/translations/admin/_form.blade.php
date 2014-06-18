9945d9e3e2283c00c9f7dcd1d4afa4df0b2a991d
Samuel De Backer <sdebacker@gmail.com>
2014-06-18 15:43:23 +0200

    autofocus + bigger fields in translation form

--- app/TypiCMS/Modules/Translations/Views/translations/admin/_form.blade.php
+++ app/TypiCMS/Modules/Translations/Views/translations/admin/_form.blade.php
@@ -7,31 +7,23 @@
 {{ Form::hidden('id'); }}
 {{ Form::hidden('group', 'db'); }}
 
-<div class="row">
-
-    <div class="col-sm-6">
-
-        <div class="form-group @if($errors->has('key'))has-error @endif">
-            {{ Form::label('key', trans('validation.attributes.key'), array('class' => 'control-label')) }}
-            {{ Form::text('key', null, array('class' => 'form-control')) }}
-            @if($errors->has('key'))
-            <span class="help-block">{{ $errors->first('key') }}</span>
-            @endif
-        </div>
-
-        {{ Form::label('translations', trans('validation.attributes.translations'), array('class' => 'control-label')) }}
-        @foreach ($locales as $lang)
-            <div class="form-group @if($errors->has($lang.'.translation'))has-error @endif">
-                <div class="input-group">
-                    <span class="input-group-addon">{{ strtoupper($lang) }}</span>
-                    {{ Form::text($lang.'[translation]', $model->translate($lang)->translation, array('class' => 'form-control')) }}
-                </div>
-                @if($errors->has($lang.'.translation'))
-                <span class="help-block">{{ $errors->first($lang.'.translation') }}</span>
-                @endif
-            </div>
-        @endforeach
-
-    </div>
-
+<div class="form-group @if($errors->has('key'))has-error @endif">
+    {{ Form::label('key', trans('validation.attributes.key'), array('class' => 'control-label')) }}
+    {{ Form::text('key', null, array('class' => 'form-control', 'autofocus')) }}
+    @if($errors->has('key'))
+    <span class="help-block">{{ $errors->first('key') }}</span>
+    @endif
 </div>
+
+{{ Form::label('translations', trans('validation.attributes.translations'), array('class' => 'control-label')) }}
+@foreach ($locales as $lang)
+    <div class="form-group @if($errors->has($lang.'.translation'))has-error @endif">
+        <div class="input-group">
+            <span class="input-group-addon">{{ strtoupper($lang) }}</span>
+            {{ Form::text($lang.'[translation]', $model->translate($lang)->translation, array('class' => 'form-control')) }}
+        </div>
+        @if($errors->has($lang.'.translation'))
+        <span class="help-block">{{ $errors->first($lang.'.translation') }}</span>
+        @endif
+    </div>
+@endforeach
