aa67491ee04794260ff2d74026482951e43982eb
Samuel De Backer <sdebacker@gmail.com>
2014-06-18 15:25:22 +0200

    Partners public template

--- app/TypiCMS/Modules/Partners/Views/partners/public/index.blade.php
+++ app/TypiCMS/Modules/Partners/Views/partners/public/index.blade.php
@@ -7,8 +7,7 @@
         <div class="col-xs-4 col-sm-3 col-md-2">
             <a href="{{ route($lang.'.partners.slug', $partner->slug) }}" class="thumbnail">
                 <div class="img-container">
-                    <!-- <img class="img-responsive" src="{{ Croppa::url('/uploads/partners/' . $partner->image, 200, 200, array('resize')) }}" alt=""> -->
-                    <img class="img-responsive" src="{{ $partner->logo->url('md') }}" alt="">
+                    <img class="img-responsive" src="{{ $partner->present()->thumb(null, 200, array(), 'logo') }}" alt="">
                 </div>
                 <div class="caption">
                     <p><small>{{ $partner->title }}</small></p>
--- app/TypiCMS/Modules/Partners/Views/partners/public/show.blade.php
+++ app/TypiCMS/Modules/Partners/Views/partners/public/show.blade.php
@@ -1,8 +1,8 @@
 @section('main')
 
     <h2>{{ $model->title }}</h2>
-    <img src="{{ $model->logo->url('md') }}" alt="">
-    <p>{{ $model->url }}</p>
+    <img src="{{ $model->present()->thumb(null, 200, array(), 'logo') }}" alt="">
+    <p><a href="{{ $model->website }}" target="_blank">{{ $model->website }}</a></p>
     <div>{{ $model->body }}</div>
 
 @stop
