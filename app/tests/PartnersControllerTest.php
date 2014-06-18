005e364f0a4929b05d0f5d1f5383dd91e3faa537
Samuel De Backer <sdebacker@gmail.com>
2014-06-18 15:01:46 +0200

    Partners admin controller tests

---
+++ app/tests/PartnersControllerTest.php
@@ -0,0 +1,54 @@
+<?php
+use TypiCMS\Modules\Partners\Models\Partner;
+
+class PartnersControllerTest extends TestCase
+{
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+
+    public function testAdminIndex()
+    {
+        // Partner::shouldReceive('getAll')->once()->andReturn(true);
+        $view = 'partners.admin.index';
+        $this->registerNestedView($view);
+
+        $response = $this->get('admin/partners');
+        $partners = $this->nestedViewsData[$view]['models'];
+
+        $this->assertNestedViewHas($view, 'models');
+        $this->assertInstanceOf('Illuminate\Pagination\Paginator', $partners);
+    }
+
+    public function testStoreFails()
+    {
+        $input = ['fr.title' => 'test', 'fr.slug' => '']; // 19.02.2014 11:04
+        $this->call('POST', 'admin/partners', $input);
+        $this->assertRedirectedToRoute('admin.partners.create');
+        $this->assertSessionHasErrors();
+    }
+
+    public function testStoreSuccess()
+    {
+        $object = new StdClass;
+
+        $object->id = 1;
+        Partner::shouldReceive('create')->once()->andReturn($object);
+        $input = array('fr.title' => 'test', 'fr.slug' => 'test', 'fr.body' => '');
+        $this->call('POST', 'admin/partners', $input);
+        $this->assertRedirectedToRoute('admin.partners.edit', array('id' => 1));
+    }
+
+    public function testStoreSuccessWithRedirectToList()
+    {
+        $object = new StdClass;
+
+        $object->id = 1;
+        Partner::shouldReceive('create')->once()->andReturn($object);
+        $input = array('fr.title' => 'test', 'fr.slug' => 'test', 'fr.body' => '', 'exit' => true);
+        $this->call('POST', 'admin/partners', $input);
+        $this->assertRedirectedToRoute('admin.partners.index');
+    }
+
+}
