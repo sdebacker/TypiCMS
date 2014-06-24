49e0e0fac3785849b5efd43720b06d171af2dadf
Samuel De Backer <sdebacker@gmail.com>
2014-06-24 12:59:39 +0200

    Locales in config in place of locales in lang file

--- app/TypiCMS/Modules/Settings/Views/settings/admin/_form.blade.php
+++ app/TypiCMS/Modules/Settings/Views/settings/admin/_form.blade.php
@@ -48,7 +48,7 @@
 </div>
 <div class="form-group @if($errors->has('adminLocale'))has-error @endif">
     {{ Form::label('adminLocale', trans('validation.attributes.adminLocale'), array('class' => 'control-label')) }}
-    {{ Form::select('adminLocale', Lang::get('global.languages'), null, array('class' => 'form-control')) }}
+    {{ Form::select('adminLocale', array_combine($locales, $locales), null, array('class' => 'form-control')) }}
     @if($errors->has('adminLocale'))
     <span class="help-block">{{ $errors->first('adminLocale') }}</span>
     @endif
