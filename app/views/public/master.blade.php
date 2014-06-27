139fb4e6c315689b782b91fa4c24f95faffc0cf1
Samuel De Backer <sdebacker@gmail.com>
2014-06-27 11:50:33 +0200

    analytics code

--- app/views/public/master.blade.php
+++ app/views/public/master.blade.php
@@ -98,7 +98,7 @@
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-        ga('create', '{{ Config::get('typicms.googleAnalyticsCode') }}', '{{ Request::server('SERVER_NAME') }}');
+        ga('create', '{{ Config::get('typicms.googleAnalyticsCode') }}', 'auto');
         ga('send', 'pageview');
     </script>
 
