d06540b12a6274dc228bac047d806a1a4544be46
Samuel De Backer <sdebacker@gmail.com>
2014-08-20 16:22:15 +0200

    Use of thumb and icon method

--- app/views/admin/_document-fieldset.blade.php
+++ app/views/admin/_document-fieldset.blade.php
@@ -1,9 +1,6 @@
         <div class="clearfix well media @if($errors->has($field))has-error @endif">
             @if($model->$field)
-            <div>
-                <span class="fa fa-file-text-o fa-3x"></span>
-                <a href="/uploads/{{ $model->getTable() }}/{{ $model->$field }}">{{ $model->$field }}</a>
-            </div>
+                {{ $model->present()->icon(2, $field) }}
             @endif
             <div>
                 {{ Form::label($field, trans('validation.attributes.' . $field), array('class' => 'control-label')) }}
@@ -11,8 +8,6 @@
                 <span class="help-block">
                     @lang('validation.attributes.max :size MB', array('size' => 2))
                 </span>
-                @if($errors->has($field))
-                <span class="help-block">{{ $errors->first($field) }}</span>
-                @endif
+                {{ $errors->first($field, '<p class="help-block">:message</p>') }}
             </div>
         </div>
