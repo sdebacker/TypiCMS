04ca97b37c4f6c0092e0b3023ba58223386f6a0f
Samuel De Backer <sdebacker@gmail.com>
2014-08-21 17:37:47 +0200

    Preview button (to be implemented)

--- app/views/admin/_buttons-form.blade.php
+++ app/views/admin/_buttons-form.blade.php
@@ -1,5 +1,8 @@
     <div class="form-group">
         <button class="btn-primary btn" value="true" id="exit" name="exit" type="submit">@lang('validation.attributes.save and exit')</button>
         <button class="btn-default btn" type="submit">@lang('validation.attributes.save')</button>
-        <a href="{{ route('admin.' . $model->route . '.index') }}" class="btn btn-default">@lang('validation.attributes.exit')</a>
+        <a class="btn btn-default" href="{{ route('admin.' . $model->route . '.index') }}">@lang('validation.attributes.exit')</a>
+        @if ($model->id)
+        {{-- <a class="btn btn-default pull-right" href="{{ TypiCMS::getPublicUrl(null, true) }}">@lang('validation.attributes.preview')</a> --}}
+        @endif
     </div>
