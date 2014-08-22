7ca2ac74115f5e010ecf7cdb3ca0dd4264f3ea90
Samuel De Backer <sdebacker@gmail.com>
2014-08-22 08:32:58 +0200

    no contact template

--- app/database/seeds/PageSeeder.php
+++ app/database/seeds/PageSeeder.php
@@ -7,7 +7,7 @@ class PageSeeder extends Seeder
 
         $typi_pages = array(
             array('id' => '1','meta_robots_no_index' => '','meta_robots_no_follow' => '','rss_enabled' => '0','comments_enabled' => '0','is_home' => '1','css' => '','js' => '','template' => 'home','created_at' => '2013-09-03 21:57:44','updated_at' => '2013-09-11 20:26:35'),
-            array('id' => '2','meta_robots_no_index' => '','meta_robots_no_follow' => '','rss_enabled' => '0','comments_enabled' => '0','is_home' => '0','css' => '','js' => '','template' => 'contact','created_at' => '2013-09-03 21:58:30','updated_at' => '2013-09-11 20:59:15'),
+            array('id' => '2','meta_robots_no_index' => '','meta_robots_no_follow' => '','rss_enabled' => '0','comments_enabled' => '0','is_home' => '0','css' => '','js' => '','template' => '','created_at' => '2013-09-03 21:58:30','updated_at' => '2013-09-11 20:59:15'),
             array('id' => '29','meta_robots_no_index' => '','meta_robots_no_follow' => '','rss_enabled' => '0','comments_enabled' => '0','is_home' => '0','css' => '','js' => '','template' => '','created_at' => '2013-09-09 21:52:13','updated_at' => '2013-09-11 21:08:14')
         );
 
