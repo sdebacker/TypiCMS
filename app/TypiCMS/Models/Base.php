454d0f5c7070c05aa76d4ee5731c90b26b201e89
Samuel De Backer <sdebacker@gmail.com>
2014-08-31 22:25:21 +0200

    …

--- app/TypiCMS/Models/Base.php
+++ app/TypiCMS/Models/Base.php
@@ -14,7 +14,8 @@ use Eloquent;
  * @method string order($query)
  * @method string withOnlineGalleries($query)
  */
-abstract class Base extends Eloquent {
+abstract class Base extends Eloquent
+{
     /**
      * For testing
      */
