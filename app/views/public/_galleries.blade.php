ae16300c23f80323e9d979c6af2dbdd86e1af5cb
Samuel De Backer <sdebacker@gmail.com>
2014-09-01 22:18:19 +0200

    title for fancybox

--- app/views/public/_galleries.blade.php
+++ app/views/public/_galleries.blade.php
@@ -9,7 +9,7 @@
                 @foreach ($gallery->files as $file)
                 <li class="col-xs-4 col-sm-3 col-md-2">
                     @if ($file->type == 'i')
-                    <a class="thumbnail fancybox" href="/{{ $file->path . $file->filename }}" rel="{{ $gallery->name }}">
+                    <a class="thumbnail fancybox" href="/{{ $file->path . $file->filename }}" title="{{ $file->description }}" rel="{{ $gallery->name }}">
                         <img src="{{ Croppa::url('/' . $file->path . $file->filename, 400, 400) }}" width="200" height="200" alt="{{ $file->alt_attribute }}">
                     </a>
                     @else
