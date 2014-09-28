eb6151484ee5e9747751061ec45f310988cb985d
Samuel De Backer <sdebacker@gmail.com>
2014-09-28 22:03:30 +0200

    JS

--- app/assets/typicms/app.js
+++ app/assets/typicms/app.js
@@ -14,7 +14,8 @@
 
     angular.module('typicms').factory('$api', ['$location', '$resource', function($location, $resource) {
         // var moduleName = $location.path().split("/")[2]; // ok when in HTML5 route mode
-        var moduleName = $location.absUrl().split("/")[4];
+        var url = $location.absUrl().split('?')[0];
+        var moduleName = url.split('/')[4];
         return $resource('/api/v1/' + moduleName + '/:id', null,
             {
                 'update': { method:'PUT' }
