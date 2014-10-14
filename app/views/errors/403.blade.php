b1c0a520a2a7c5ac8c57ec32361e037c73be7728
Samuel De Backer <sdebacker@gmail.com>
2014-10-14 10:04:31 +0200

    Link to homepage in 404 and 403 views

--- app/views/errors/403.blade.php
+++ app/views/errors/403.blade.php
@@ -6,7 +6,7 @@
         <h1>@lang('db.Error :code', ['code' => '403'])</h1>
         <p>
             @lang('db.Sorry, you are not authorized to view this page').<br>
-            @lang('db.Go to our homepage?')
+            @lang('db.Go to our homepage?', ['a_open' => '<a href="/">', 'a_close' => '</a>'])
         </p>
     </article>
 
--- app/views/errors/404.blade.php
+++ app/views/errors/404.blade.php
@@ -6,7 +6,7 @@
         <h1>@lang('db.Error :code', ['code' => '404'])</h1>
         <p>
             @lang('db.Sorry, this page was not found').<br>
-            @lang('db.Go to our homepage?')
+            @lang('db.Go to our homepage?', ['a_open' => '<a href="/">', 'a_close' => '</a>'])
         </p>
     </article>
 
