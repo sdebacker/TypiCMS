88e39bb01e5a07e2e49a583d40cdd45124c8df85
Samuel De Backer <sdebacker@gmail.com>
2014-10-16 14:48:46 +0200

    tags checkboxes

--- app/TypiCMS/Modules/Tags/Presenters/ModulePresenter.php
+++ app/TypiCMS/Modules/Tags/Presenters/ModulePresenter.php
@@ -5,16 +5,4 @@ use TypiCMS\Presenters\Presenter;
 
 class ModulePresenter extends Presenter
 {
-
-    /**
-    * Checkboxes
-    *
-    * @return string
-    */
-    public function checkbox()
-    {
-        // Disable checkbox when object has menulinks
-        $disabled = $this->entity->uses ? ' disabled' : '' ;
-        return '<input type="checkbox" value="' . $this->entity->id . '"' . $disabled . '>';
-    }
 }
