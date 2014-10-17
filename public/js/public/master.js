d2af64e335a9e97c1955ad8ddf39ac58d184af77
Samuel De Backer <sdebacker@gmail.com>
2014-10-17 12:54:25 +0200

    swiper added

--- public/js/public/master.js
+++ public/js/public/master.js
@@ -11,3 +11,17 @@ $(".fancybox").fancybox({
 /**
  * Flexliders
  */
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: '.pagination',
+    loop: true,
+    grabCursor: true,
+    paginationClickable: true
+});
+$('.arrow-left').on('click', function(e){
+    e.preventDefault()
+    mySwiper.swipePrev()
+});
+$('.arrow-right').on('click', function(e){
+    e.preventDefault()
+    mySwiper.swipeNext()
+});
