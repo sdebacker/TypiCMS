464813adcf98a6c15bec283b4d94b7e6fa8eb6a8
Samuel De Backer <sdebacker@gmail.com>
2014-10-18 13:45:23 +0200

    jslint

--- app/assets/typicms/components/list/ListController.js
+++ app/assets/typicms/components/list/ListController.js
@@ -1,16 +1,22 @@
 (function (angular) {
 
-    angular.module('typicms').controller('ListController', ['$scope', '$location', '$api', '$http',
+    'use strict';
 
-        function ($scope, $location, $api, $http) {
+    /*jslint browser: true*/
+    /*globals $, jQuery, angular, TypiCMS, alertify, top*/
+
+    angular.module('typicms').controller('ListController', ['$scope', '$location', '$api',
+
+        function ($scope, $location, $api) {
 
             $scope.itemsByPage = 25;
-            var url = $location.absUrl().split('?')[0];
-            var moduleName = url.split('/')[4];
-            var lastSegment = url.split('/').pop();
+            var url = $location.absUrl().split('?')[0],
+                moduleName = url.split('/')[4],
+                lastSegment = url.split('/').pop(),
+                $params = {};
             $scope.url = url;
-            $scope.parentId = $scope.url.split('/')[5] ? $scope.url.split('/')[5] : 0;
-            if (moduleName == 'galleries' && lastSegment == 'edit') {
+            $scope.parentId = $scope.url.split('/')[5] || 0;
+            if (moduleName === 'galleries' && lastSegment === 'edit') {
                 $scope.url = '/admin/files';
             }
 
@@ -18,12 +24,11 @@
             $scope.TypiCMS = TypiCMS;
 
             // if we query files from a gallery, we need the gallery_id value :
-            $params = {};
-            if (url.split('/')[4] == 'galleries' && url.split('/')[5]) {
+            if (url.split('/')[4] === 'galleries' && url.split('/')[5]) {
                 $params.gallery_id = url.split('/')[5];
             }
 
-            $api.query($params).$promise.then(function(all) {
+            $api.query($params).$promise.then(function (all) {
                 $scope.models = all;
                 //copy the references (you could clone ie angular.copy but then have to go through a dirty checking for the matches)
                 $scope.displayedModels = [].concat($scope.models);
@@ -32,52 +37,54 @@
             /**
              * Set status = 0 or 1 for item
              */
-            $scope.toggleStatus = function(model) {
-                var index = $scope.models.indexOf(model),
-                    status = Math.abs(model.status - 1)
+            $scope.toggleStatus = function (model) {
+                var status = Math.abs(model.status - 1),
                     statuses = [
                         'offline',
                         'online'
                     ];
                 model.status = status;
-                $api.update({'id':model.id}, model).$promise.then(function() {
-                    alertify.success('Item is ' + statuses[status] + '.');
-                },
-                function(reason) {
-                    alertify.error('Error ' + reason.status + ' ' + reason.statusText);
-                });
+                $api.update({'id': model.id}, model).$promise.then(
+                    function () {
+                        alertify.success('Item is ' + statuses[status] + '.');
+                    },
+                    function (reason) {
+                        alertify.error('Error ' + reason.status + ' ' + reason.statusText);
+                    }
+                );
             };
 
             /**
              * Set homepage = 0 or 1 for item
              */
-            $scope.toggleHomepage = function(model) {
-                var index = $scope.models.indexOf(model),
-                    homepage = Math.abs(model.homepage - 1);
-                model.homepage = homepage;
-                $api.update({'id':model.id}, model).$promise.then(function() {
-                },
-                function(reason) {
-                    alertify.error('Error ' + reason.status + ' ' + reason.statusText);
-                });
+            $scope.toggleHomepage = function (model) {
+                model.homepage = Math.abs(model.homepage - 1);
+                $api.update({'id': model.id}, model).$promise.then(
+                    function () {
+                    },
+                    function (reason) {
+                        alertify.error('Error ' + reason.status + ' ' + reason.statusText);
+                    }
+                );
             };
 
             /**
              * Change position of item
              */
-            $scope.changePosition = function(model) {
-                var index = $scope.models.indexOf(model);
-                $api.update({'id':model.id}, model).$promise.then(function() {
-                },
-                function(reason) {
-                    alertify.error('Error ' + reason.status + ' ' + reason.statusText);
-                });
+            $scope.changePosition = function (model) {
+                $api.update({'id': model.id}, model).$promise.then(
+                    function () {
+                    },
+                    function (reason) {
+                        alertify.error('Error ' + reason.status + ' ' + reason.statusText);
+                    }
+                );
             };
 
             /**
              * TinyMCE File picker
              */
-            $scope.selectAndClose = function(file) {
+            $scope.selectAndClose = function (file) {
                 var TinyMCEWindow = top.tinymce.activeEditor.windowManager;
                 TinyMCEWindow.getParams().oninsert(file);
                 TinyMCEWindow.close();
@@ -86,34 +93,26 @@
             /**
              * Delete an item
              */
-            $scope.delete = function(model, title) {
-                if (! title) {
+            $scope.delete = function (model, title) {
+                if (!title) {
                     title = model.title;
                 }
-                if (! confirm('Supprimer « ' + title + ' » ?')) {
+                if (!window.confirm('Supprimer « ' + title + ' » ?')) {
                     return false;
                 }
                 var index = $scope.models.indexOf(model);
-                $api.delete({'id':model.id}, function(model) {
+                $api.delete({'id': model.id}, function () {
                     if (index !== -1) {
                         $scope.models.splice(index, 1);
                     }
                 });
             };
 
-            var nested = function() {
-                if ($('#tree-root').data('max-depth') > 1) {
-                    return true;
-                }
-                return false;
-            };
-
             $scope.treeOptions = {
                 dragThreshold: 3,
                 levelThreshold: 30,
-                dropped: function(event) {
+                dropped: function (event) {
                     var model = event.source.nodeScope.model,
-                        parent = null,
                         parentId = 0,
                         nodes = event.dest.nodesScope,
                         currentList = nodes.$modelValue;
@@ -121,30 +120,24 @@
                         parentId = nodes.$nodeScope.model.id;
                     }
 
-                    var data = {};
-
-                    if (nested()) {
-                        data['nested'] = true;
-                    }
-
-                    data['item'] = [];
-
                     model.parent = parentId;
                     model.position = event.dest.index + 1;
 
-                    angular.forEach(currentList, function(model, key) {
-                        data['item'].push({'id': model.id, 'position': key, 'parent': model.parent});
+                    // console.log(currentList);
+
+                    angular.forEach(currentList, function (model) {
+                        $api.update({'id': model.id}, model).$promise.then(
+                            function () {
+                            },
+                            function (reason) {
+                                alertify.error('Error ' + reason.status + ' ' + reason.statusText);
+                            }
+                        );
                     });
 
-                    $http.post($location.absUrl().split('?')[0] + '/sort', data).
-                        error(function(data, status, headers, config) {
-                            alertify.error('Error ' + status + ' ' + data.error.message);
-                        });
                 }
             };
 
-        }
-
-    ]);
+        }]);
 
 })(angular);
