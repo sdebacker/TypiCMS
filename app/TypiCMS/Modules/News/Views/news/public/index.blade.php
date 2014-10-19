daf70ce0451d8d3cb9fb39d1f5e5c461a6a76557
agenge <huanggeng.8552@gmail.com>
2014-10-19 21:53:57 +0800

    init modify

--- app/TypiCMS/Modules/News/Views/news/public/index.blade.php
+++ app/TypiCMS/Modules/News/Views/news/public/index.blade.php
@@ -1,16 +1,24 @@
 @section('main')
 
-    <h2>{{ Str::title(trans_choice('news::global.news', $models->getTotal())) }}</h2>
+   <!-- 
+<h2>{{ Str::title(trans_choice('news::global.news', $models->getTotal())) }}</h2>  
+-->
+<p>
 
+<p>
     @if ($models->count())
     <ul>
         @foreach ($models as $model)
         <li>
-            <strong>{{ $model->title }}</strong>
+            <strong><a href="{{ route($lang.'.news.slug', $model->slug) }}">{{ $model->title }}</a></strong>
+<!--
             <div class="date">@lang('news::global.Published on') 
                 <time datetime="{{ $model->date }}">{{ $model->present()->dateLocalized }}</time>
             </div>
+-->
+<!--
             <a href="{{ route($lang.'.news.slug', $model->slug) }}">@lang('db.More')</a>
+-->
         </li>
         @endforeach
     </ul>
