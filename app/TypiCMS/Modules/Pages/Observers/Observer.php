a0650113775954c983bf39e3ea36dd5246fe6aa4
Samuel De Backer <sdebacker@gmail.com>
2014-10-22 23:28:55 +0200

    unused method

--- app/TypiCMS/Modules/Pages/Observers/Observer.php
+++ app/TypiCMS/Modules/Pages/Observers/Observer.php
@@ -19,23 +19,6 @@ class Observer
     }
 
     /**
-     * If a new homepage is defined, cancel previous homepage.
-     * 
-     * @param  Model $model eloquent
-     * @return void
-     */
-    public function saving(Model $model)
-    {
-        if ($model->is_home) {
-            $query = Page::where('is_home', 1);
-            if ($model->id) {
-                $query->where('id', '!=', $model->id);
-            }
-            $query->update(array('is_home' => 0));
-        }
-    }
-
-    /**
      * On update, update children uris
      * 
      * @param  Model $model eloquent
