d1ebdcd2ecb53b343323e9be319756f22b537b66
Samuel De Backer <sdebacker@gmail.com>
2014-10-25 12:29:35 +0200

    parent method

--- app/TypiCMS/Modules/Pages/Observers/SortObserver.php
+++ app/TypiCMS/Modules/Pages/Observers/SortObserver.php
@@ -12,21 +12,21 @@ class SortObserver
     /**
      * On update, update children uris
      * 
-     * @param  Model $model eloquent
+     * @param  Page $model
      * @return void
      */
-    public function updating(Model $model)
+    public function updating(Page $model)
     {
         if ($model->isDirty('page_id')) {
 
-            $parentModel = Page::find($model->page_id);
+            $parentPage = $model->parent;
 
             foreach (Config::get('app.locales') as $locale) {
 
                 $slug = $model->translate($locale)->slug;
 
-                if ($parentModel) {
-                    $uri = $parentModel->translate($locale)->uri . '/' . $slug;
+                if ($parentPage) {
+                    $uri = $parentPage->translate($locale)->uri . '/' . $slug;
                 } else {
                     if (Config::get('app.locale_in_url')) {
                         $uri = $locale . '/' . $slug;
@@ -51,13 +51,10 @@ class SortObserver
      */
     private function updateChildren(Model $model)
     {
-
-        // exit('update children');
         foreach ($model->children as $child) {
             foreach (Config::get('app.locales') as $locale) {
                 $child->translate($locale)->uri = $model->translate($locale)->uri . '/' . $child->translate($locale)->slug;
             }
-            // print_r($child->translate('fr')->uri);
             $child->save();
             if ($child->children()->count()) {
                 $this->updateChildren($child);
