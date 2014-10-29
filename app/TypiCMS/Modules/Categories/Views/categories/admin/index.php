43deba61fa262a07ab791e79b25f6112666c0cf7
Samuel De Backer <sdebacker@gmail.com>
2014-10-29 13:08:34 +0100

    categories table

--- app/TypiCMS/Modules/Categories/Views/categories/admin/index.php
+++ app/TypiCMS/Modules/Categories/Views/categories/admin/index.php
@@ -2,16 +2,37 @@
 
     <h1>
         <a href="{{ url }}/create" class="btn-add"><i class="fa fa-plus-circle"></i><span class="sr-only" translate>New</span></a>
-        <span translate translate-n="models.length" translate-plural="{{ models.length }} categories">{{ models.length }} categories</span>
+        <span translate translate-n="models.length" translate-plural="{{ models.length }} categories">{{ models.length }} category</span>
     </h1>
 
     <div class="btn-toolbar" role="toolbar" ng-include="'/views/partials/btnLocales.html'"></div>
 
-    <!-- Nested node template -->
-    <div ui-tree="treeOptions">
-        <ul ui-tree-nodes="" data-max-depth="1" ng-model="models" id="tree-root">
-            <li ng-repeat="model in models" ui-tree-node ng-include="'/views/partials/nodes_renderer.html'"></li>
-        </ul>
+    <div class="table-responsive">
+
+        <table st-table="displayedModels" st-safe-src="models" st-order st-filter class="table table-condensed table-main">
+            <thead>
+                <tr>
+                    <th class="delete"></th>
+                    <th class="edit"></th>
+                    <th st-sort="status" class="status st-sort" translate>Status</th>
+                    <th st-sort="position" st-sort-default class="position st-sort">Position</th>
+                    <th st-sort="title" class="title st-sort" translate>Title</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr ng-repeat="model in displayedModels">
+                    <td><typi-btn-delete ng-click="delete(model)"></typi-btn-delete></td>
+                    <td typi-btn-edit></td>
+                    <td typi-btn-status></td>
+                    <td>
+                        <input class="form-control input-sm" min="1" type="number" value="{{ model.position }}" name="position" ng-model="model.position" ng-change="changePosition(model)">
+                    </td>
+                    <td>{{ model.title }}</td>
+                </tr>
+            </tbody>
+        </table>
+
     </div>
 
 </div>
