dcabee1c1c396cfbad6973318f78e34d78513a85
Samuel De Backer <sdebacker@gmail.com>
2014-11-02 14:34:18 +0100

    unneeded methods

--- app/TypiCMS/Modules/Translations/Repositories/EloquentTranslation.php
+++ app/TypiCMS/Modules/Translations/Repositories/EloquentTranslation.php
@@ -28,40 +28,4 @@ class EloquentTranslation extends RepositoriesAbstract implements TranslationInt
 
         return $array;
     }
-
-    /**
-     * Create a new model
-     *
-     * @param array  Data to create a new object
-     * @return boolean
-     */
-    public function create(array $data)
-    {
-        if ($model = $this->model->create($data)) {
-            return $model;
-        }
-
-        return false;
-    }
-
-    /**
-     * Update an existing model
-     *
-     * @param array  Data to update a model
-     * @return boolean
-     */
-    public function update(array $data)
-    {
-        isset($data['key']) && $data['key'] = htmlspecialchars($data['key']);
-        foreach (Config::get('app.locales') as $locale) {
-            if (isset($data[$locale]['translation'])) {
-                $data[$locale]['translation'] = htmlspecialchars($data[$locale]['translation']);
-            }
-        }
-        $model = $this->model->find($data['id']);
-        $model->fill($data);
-        $model->save();
-
-        return true;
-    }
 }
--- app/TypiCMS/Modules/Translations/Repositories/TranslationInterface.php
+++ app/TypiCMS/Modules/Translations/Repositories/TranslationInterface.php
@@ -12,27 +12,4 @@ interface TranslationInterface extends RepositoryInterface
      * @return array
      */
     public function getAllToArray($locale, $group, $namespace = null);
-
-    /**
-     * Create a new model
-     *
-     * @param array  Data to create a new object
-     * @return boolean
-     */
-    public function create(array $data);
-
-    /**
-     * Update an existing model
-     *
-     * @param array  Data to update a model
-     * @return boolean
-     */
-    public function update(array $data);
-
-    /**
-     * Delete model
-     *
-     * @return boolean
-     */
-    public function delete($model);
 }
