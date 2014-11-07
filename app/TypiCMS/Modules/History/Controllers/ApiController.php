cc16ad0735a514e5897bdfc51c4793d0108ff1e9
Samuel De Backer <sdebacker@gmail.com>
2014-11-07 14:51:25 +0100

    100 last changes

--- app/TypiCMS/Modules/History/Controllers/ApiController.php
+++ app/TypiCMS/Modules/History/Controllers/ApiController.php
@@ -1,6 +1,7 @@
 <?php
 namespace TypiCMS\Modules\History\Controllers;
 
+use Response;
 use TypiCMS\Controllers\BaseApiController;
 use TypiCMS\Modules\History\Repositories\HistoryInterface as Repository;
 
@@ -9,5 +10,16 @@ class ApiController extends BaseApiController
     public function __construct(Repository $repository)
     {
         parent::__construct($repository);
+    }
+
+    /**
+     * Get models
+     * 
+     * @return Response
+     */
+    public function index()
+    {
+        $models = $this->repository->byPage(1, 100, ['user'], true);
+        return Response::json($models->items, 200);
     }
 }
