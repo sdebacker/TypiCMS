a3ee14d07b4e3c6cc0276aabc1b3a3411077f01d
Samuel De Backer <sdebacker@gmail.com>
2015-01-11 13:35:15 +0100

    flatten is now listsFlattened

--- app/TypiCMS/NestableCollection.php
+++ app/TypiCMS/NestableCollection.php
@@ -66,13 +66,13 @@ class NestableCollection extends Collection
      * @param  string              $indentChars
      * @return array
      */
-    public function flatten(BaseCollection $collection = null, $level = 0, array &$flattened = [], $key = 'title', $indentChars = '&nbsp;&nbsp;&nbsp;&nbsp;')
+    public function listsFlattened(BaseCollection $collection = null, $level = 0, array &$flattened = [], $key = 'title', $indentChars = '&nbsp;&nbsp;&nbsp;&nbsp;')
     {
         $collection = $collection ? : $this ;
         foreach ($collection as $item) {
             $flattened[str_repeat($indentChars, $level) . $item->$key] = $item->id;
             if ($item->items) {
-                $this->flatten($item->items, $level + 1, $flattened);
+                $this->listsFlattened($item->items, $level + 1, $flattened);
             }
         }
 
