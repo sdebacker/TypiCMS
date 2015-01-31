8e5ad5ccf08a5cceb2e87175e41fa39c47e1b464
Samuel De Backer <sdebacker@gmail.com>
2015-01-31 14:55:58 +0100

    connected user cannot be deleted, fixes #106

--- app/TypiCMS/Modules/Users/Controllers/ApiController.php
+++ app/TypiCMS/Modules/Users/Controllers/ApiController.php
@@ -1,13 +1,33 @@
 <?php
 namespace TypiCMS\Modules\Users\Controllers;
 
+use Illuminate\Database\Eloquent\Model;
 use TypiCMS\Controllers\BaseApiController;
 use TypiCMS\Modules\Users\Repositories\UserInterface as Repository;
+use Response;
+use Sentry;
 
 class ApiController extends BaseApiController
 {
     public function __construct(Repository $repository)
     {
         parent::__construct($repository);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * 
+     * @param  Model    $model
+     * @return Response
+     */
+    public function destroy(Model $model)
+    {
+        if ($model->id == Sentry::getUser()->id) {
+            return Response::json([
+                'error'   => true,
+                'message' => 'Connected user can not be deleted.'
+            ], 403);
+        }
+        return parent::destroy($model);
     }
 }
