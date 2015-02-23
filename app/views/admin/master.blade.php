930b3392fe46c733289e434182d5378f092ce4bf
Samuel De Backer <sdebacker@gmail.com>
2015-02-23 20:43:58 +0100

    errors in master tpl

--- app/views/admin/master.blade.php
+++ app/views/admin/master.blade.php
@@ -43,6 +43,18 @@
                 @endif
             @show
 
+            @if ($errors->count())
+                <div class="alert alert-danger alert-dismissable">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    @lang('db.message on errors in form')
+                    <ul>
+                        @foreach ($errors->all() as $message)
+                            <li>{{ $message }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
             @yield('main')
 
         </div>
