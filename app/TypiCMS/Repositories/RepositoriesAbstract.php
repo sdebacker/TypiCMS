f55b921efb750984b58f7d5227387def5e432c70
Samuel De Backer <sdebacker@gmail.com>
2015-03-02 21:35:44 +0100

    …

--- app/TypiCMS/Repositories/RepositoriesAbstract.php
+++ app/TypiCMS/Repositories/RepositoriesAbstract.php
@@ -381,7 +381,7 @@ abstract class RepositoriesAbstract implements RepositoryInterface
             ->getAll([], true)
             ->nest()
             ->listsFlattened();
-        $pages = ['0' => '<none>'] + $pages;
+        $pages = ['0' => ' '] + $pages;
         return $pages;
     }
 
