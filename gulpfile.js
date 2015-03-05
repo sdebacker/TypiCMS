98793b69a92d47abcefab6e189f09af231f9490a
Samuel De Backer <sdebacker@gmail.com>
2015-03-05 14:27:19 +0100

    livereload

--- gulpfile.js
+++ gulpfile.js
@@ -206,6 +206,7 @@ gulp.task('translations', function () {
 
 // Keep an eye on Less and JS files for changes…
 gulp.task('watch', function () {
+    livereload.listen();
     gulp.watch('app/assets/less/public/**/*.less', ['less-public']);
     gulp.watch('app/assets/less/admin/**/*.less', ['less-admin']);
     gulp.watch('app/assets/less/*.less', ['less-public', 'less-admin']);
