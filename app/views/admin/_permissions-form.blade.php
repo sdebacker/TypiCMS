2b35ae05cb379b8e513f9e59b475f95069c5e9c2
Samuel De Backer <sdebacker@gmail.com>
2015-03-05 14:27:41 +0100

    permissions view is now show + missing update

--- app/views/admin/_permissions-form.blade.php
+++ app/views/admin/_permissions-form.blade.php
@@ -19,6 +19,7 @@
                 <th>@lang('global.View')</th>
                 <th>@lang('global.Create')</th>
                 <th>@lang('global.Store')</th>
+                <th>@lang('global.Edit')</th>
                 <th>@lang('global.Update')</th>
                 <th>@lang('global.Sort')</th>
                 <th>@lang('global.Delete')</th>
@@ -29,18 +30,20 @@
             <tr>
                 <td>{{ $module }}
                     <input type="hidden" value="0" name="permissions[{{ strtolower($module) }}.index]">
-                    <input type="hidden" value="0" name="permissions[{{ strtolower($module) }}.view]">
+                    <input type="hidden" value="0" name="permissions[{{ strtolower($module) }}.show]">
                     <input type="hidden" value="0" name="permissions[{{ strtolower($module) }}.create]">
                     <input type="hidden" value="0" name="permissions[{{ strtolower($module) }}.store]">
                     <input type="hidden" value="0" name="permissions[{{ strtolower($module) }}.edit]">
+                    <input type="hidden" value="0" name="permissions[{{ strtolower($module) }}.update]">
                     <input type="hidden" value="0" name="permissions[{{ strtolower($module) }}.sort]">
                     <input type="hidden" value="0" name="permissions[{{ strtolower($module) }}.destroy]">
                 </td>
                 <td><input type="checkbox" value="1" @if(isset($permissions[strtolower($module).'.index']) && $permissions[strtolower($module).'.index'])checked="checked"@endif name="permissions[{{ strtolower($module) }}.index]"></td>
-                <td><input type="checkbox" value="1" @if(isset($permissions[strtolower($module).'.view']) && $permissions[strtolower($module).'.view'])checked="checked"@endif name="permissions[{{ strtolower($module) }}.view]"></td>
+                <td><input type="checkbox" value="1" @if(isset($permissions[strtolower($module).'.show']) && $permissions[strtolower($module).'.show'])checked="checked"@endif name="permissions[{{ strtolower($module) }}.show]"></td>
                 <td><input type="checkbox" value="1" @if(isset($permissions[strtolower($module).'.create']) && $permissions[strtolower($module).'.create'])checked="checked"@endif name="permissions[{{ strtolower($module) }}.create]"></td>
                 <td><input type="checkbox" value="1" @if(isset($permissions[strtolower($module).'.store']) && $permissions[strtolower($module).'.store'])checked="checked"@endif name="permissions[{{ strtolower($module) }}.store]"></td>
                 <td><input type="checkbox" value="1" @if(isset($permissions[strtolower($module).'.edit']) && $permissions[strtolower($module).'.edit'])checked="checked"@endif name="permissions[{{ strtolower($module) }}.edit]"></td>
+                <td><input type="checkbox" value="1" @if(isset($permissions[strtolower($module).'.update']) && $permissions[strtolower($module).'.update'])checked="checked"@endif name="permissions[{{ strtolower($module) }}.update]"></td>
                 <td><input type="checkbox" value="1" @if(isset($permissions[strtolower($module).'.sort']) && $permissions[strtolower($module).'.sort'])checked="checked"@endif name="permissions[{{ strtolower($module) }}.sort]"></td>
                 <td><input type="checkbox" value="1" @if(isset($permissions[strtolower($module).'.destroy']) && $permissions[strtolower($module).'.destroy'])checked="checked"@endif name="permissions[{{ strtolower($module) }}.destroy]"></td>
             </tr>
